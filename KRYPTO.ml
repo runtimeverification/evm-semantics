edd6727b7d07e4b4c2c7477f0d142efc9c3c24bb
Dwight Guth <dwight.guth@runtimeverification.com>
2017-06-27 09:33:46 -0500

    minor fixes to keccak

--- KRYPTO.ml
+++ KRYPTO.ml
@@ -2,5 +2,9 @@ open Constants.K
 
 let h = Cryptokit.Hash.keccak 256
 
-let hook_keccak256 c lbl sort config ff = match c with
-  [String input] -> [String (Cryptokit.hash_string h input)]
+let hook_keccak256 c lbl sort config ff = 
+  let buf = Buffer.create 64 in
+  let bytes = match c with
+    [String input] -> Cryptokit.hash_string h input in
+  String.iter (fun c -> Buffer.add_string buf (Printf.sprintf "%x" (int_of_char c))) bytes;
+  [String (Buffer.contents buf)]
