2a843fb22df358440b4300cc67705049c2594812
Everett Hildenbrandt <hildenb2@illinois.edu>
2017-10-14 10:13:54 -0500

    ethereum => driver, evm, README, Makefile, {vm,blockchain}test, docs/: more appropriate file name

--- docs/compile.sh
+++ docs/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pandoc -o analysis.rst  analysis.md 
 pandoc -o data.rst data.md
-pandoc -o ethereum.rst ethereum.md
+pandoc -o driver.rst driver.md
 pandoc -o evm.rst evm.md
 pandoc -o issues.rst issues.md
 pandoc -o krypto.rst krypto.md
