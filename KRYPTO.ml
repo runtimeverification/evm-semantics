9ccfa5ea67aee44239ed79c9068b39c9405daa72
Dwight Guth <dwight.guth@runtimeverification.com>
2017-11-15 11:15:23 -0600

    KRYPTO.ml, META, Makefile, krypto.md: add bn128 ecadd, ecmul, and ecpairing operations to KRYPTO module

--- KRYPTO.ml
+++ KRYPTO.ml
@@ -1,3 +1,4 @@
+open Constants
 open Constants.K
 
 let do_hash str h =
@@ -48,3 +49,62 @@ let hook_ecdsaRecover c lbl sort config ff = match c with
   with Failure _ -> [String ""]
   |    Z.Overflow -> [String ""])
 | _ -> failwith "ecdsaRecover"
+
+exception InvalidPoint
+
+let get_pt k = match k with
+| [KApply2(Lbl'LPar'_'Comm'_'RPar'_KRYPTO, [Int x], [Int y])] ->
+    if Z.equal x Z.zero && Z.equal y Z.zero then BN128Curve.Infinite else
+    if (Z.lt x BN128Elements.field_modulus && Z.lt y BN128Elements.field_modulus) then
+      let pt = BN128Curve.Finite (BN128Elements.FQ.create x, BN128Elements.FQ.create y) in
+      if BN128Curve.G1.is_on_curve pt then pt else raise InvalidPoint
+    else raise InvalidPoint
+| _ -> invalid_arg "not a point"
+
+let get_pt_g2 k = match k with
+| [KApply4(Lbl'LPar'_x_'Comm'_x_'RPar'_KRYPTO, [Int x1], [Int x2], [Int y1], [Int y2])] ->
+    if Z.equal x1 Z.zero && Z.equal x2 Z.zero && Z.equal y1 Z.zero && Z.equal y2 Z.zero then BN128Curve.Infinite else
+    if (Z.lt x1 BN128Elements.field_modulus && Z.lt x2 BN128Elements.field_modulus && Z.lt y1 BN128Elements.field_modulus && Z.lt y2 BN128Elements.field_modulus) then
+      let pt = BN128Curve.Finite (BN128Elements.FQP.create_fq2 [|x1; x2|], BN128Elements.FQP.create_fq2 [|y1; y2|]) in
+      if BN128Curve.G2.is_on_curve pt then pt else raise InvalidPoint
+    else raise InvalidPoint
+| _ -> invalid_arg "not a point"
+
+let project_pt pt = let x, y = match pt with
+| BN128Curve.Finite (xf, yf) -> BN128Elements.FQ.to_z xf, BN128Elements.FQ.to_z yf
+| BN128Curve.Infinite -> Z.zero, Z.zero
+in [KApply2(Lbl'LPar'_'Comm'_'RPar'_KRYPTO, [Int x], [Int y])]
+
+let hook_bn128valid c lbl sort config ff =
+  try
+    let _ = get_pt c in
+    [Bool true]
+  with InvalidPoint -> [Bool false]
+
+let hook_bn128g2valid c lbl sort config ff =
+  try
+    let _ = get_pt_g2 c in
+    [Bool true]
+  with InvalidPoint -> [Bool false]
+
+let hook_bn128add c lbl sort config ff = match c with
+  k1, k2 ->
+  let pt1 = get_pt k1 in
+  let pt2 = get_pt k2 in
+  project_pt (BN128Curve.G1.add pt1 pt2)
+
+let hook_bn128mul c lbl sort config ff = match c with
+  k,
+  [Int s] ->
+  let pt1 = get_pt k in
+  project_pt (BN128Curve.G1.mul pt1 s)
+| _ -> failwith "bn128mul"
+
+let hook_bn128ate c lbl sort config ff = match c with
+  [List(_, _, a)], [List(_, _, b)] ->
+  let g1a = List.map get_pt a in
+  let g2b = List.map get_pt_g2 b in
+  let fq12c = List.map BN128Pairing.pairing (List.combine g2b g1a) in
+  let product = List.fold_left BN128Elements.FQP.mulp BN128Elements.FQP.one_fq12 fq12c in
+  [Bool (product = BN128Elements.FQP.one_fq12)]
+| _ -> failwith "bn128ate"
--- krypto.md
+++ krypto.md
@@ -19,5 +19,28 @@ module KRYPTO
                     | ECDSARecover ( String , Int , String , String ) [function, hook(KRYPTO.ecdsaRecover)]
                     | Sha256 ( String )                               [function, hook(KRYPTO.sha256)]
                     | RipEmd160 ( String )                            [function, hook(KRYPTO.ripemd160)]
+ // ----------------------------------------------------------------------------------------------------
+```
+
+The BN128 elliptic curve is defined over 2-dimensional points over the fields of zero- and first-degree polynomials modulo a large prime. (x, y) is a point on G1, whereas (x1 x x2, y1 x y2) is a point on G2, in which x1 and y1 are zero-degree coefficients and x2 and y2 are first-degree coefficients. In each case, (0, 0) is used to represent the point at infinity.
+
+-   `BN128Add` adds two points in G1 together,
+-   `BN128Mul` multiplies a point in G1 by a scalar.
+-   `BN128AtePairing` accepts a list of points in G1 and a list of points in G2 and returns whether the sum of the product of the discrete logarithm of the G1 points multiplied by the discrete logarithm of the G2 points is equal to zero.
+-   `isValidPoint` takes a point in either G1 or G2 and validates that it actually falls on the respective elliptic curve.
+
+```{.k .uiuck .rvk}
+    syntax G1Point ::= "(" Int "," Int ")"
+    syntax G2Point ::= "(" Int "x" Int "," Int "x" Int ")"
+    syntax G1Point ::= BN128Add(G1Point, G1Point) [function, hook(KRYPTO.bn128add)]
+                     | BN128Mul(G1Point, Int)     [function, hook(KRYPTO.bn128mul)]
+ // -------------------------------------------------------------------------------
+
+    syntax Bool ::= BN128AtePairing(List, List) [function, hook(KRYPTO.bn128ate)]
+ // -----------------------------------------------------------------------------
+
+    syntax Bool ::= isValidPoint(G1Point) [function, hook(KRYPTO.bn128valid)]
+                  | isValidPoint(G2Point) [function, klabel(isValidG2Point), hook(KRYPTO.bn128g2valid)]
+ // ---------------------------------------------------------------------------------------------------
 endmodule
 ```
