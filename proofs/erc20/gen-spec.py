ecfe56fa49e6788cf7c63cdf9c4d8e641e34a3a5
Nishant Rodrigues <nishantjr@gmail.com>
2018-01-11 14:32:08 +0530

    gen-spec: Touch a timestamp file to simplify Makefile

--- proofs/erc20/gen-spec.py
+++ proofs/erc20/gen-spec.py
@@ -12,14 +12,20 @@ def gen(template, ini):
     config = configparser.ConfigParser()
     config.read(ini)
     sections = ['DEFAULT'] if not config.sections() else config.sections()
+    out_prefix = os.path.splitext(ini)[0] 
     for sec in sections:
         genspec = template
         for key in config[sec]:
             genspec = subst(genspec, key, config[sec][key].strip())
         genspec = subst(genspec, 'module', os.path.splitext(os.path.basename(ini))[0].upper())
-        fout = open(os.path.splitext(ini)[0] + ('' if sec == 'DEFAULT' else sec) + "-spec.k", "w")
+        fout = open(out_prefix + ('' if sec == 'DEFAULT' else sec) + "-spec.k", "w")
         fout.write(genspec)
         fout.close()
+
+    # touch timestamp file for simplifying makefile
+    timestamp_file = out_prefix + '.timestamp'
+    with open(timestamp_file, 'a'):
+        os.utime(timestamp_file, None)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
