97525a7576f8e4a1ac864a2e365c8c437063edce
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-01-16 11:06:54 -0700

    docs/{conf.py,Makefile}: setup Sphinx to read markdown directly

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -76,6 +76,11 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
+
+# -- Options for Markdown input -------------------------------------------
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
 
 
 # -- Options for HTML output ----------------------------------------------
