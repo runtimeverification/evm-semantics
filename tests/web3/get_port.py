0961335ac3a4c7cc5b69be197e18a6fa26d4ac86
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-08-14 12:55:57 -0600

    tests/web3/get_port: use python3

--- tests/web3/get_port.py
+++ tests/web3/get_port.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 0))
 addr = s.getsockname()
-print addr[1]
+print(str(addr[1]))
 s.close()
