0f76b0e2f1b5ef14a28b241eae3c62bb5de60aee
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-08-22 10:32:41 -0600

    tests/web3/runtest.sh: make sure client is really dead no matter what

--- tests/web3/runtest.sh
+++ tests/web3/runtest.sh
@@ -7,7 +7,8 @@ output_file="$1" ; shift
 
 # Start Firefly
 PORT=$(tests/web3/get_port.py)
-./kevm web3 "$PORT" &
+./kevm web3 "$PORT" "$@" &
+kevm_client_pid="$!"
 while ! netcat -z 127.0.0.1 "$PORT"; do sleep 0.1; done
 
 # Feed input in, store output in tmp file
@@ -18,5 +19,6 @@ cat "$input_file" | netcat 127.0.0.1 "$PORT" -q 0 > "$tmp_output_file"
 exit_code='0'
 git --no-pager diff --no-index "$output_file" "$tmp_output_file" || exit_code="$?"
 ./kevm web3-send "$PORT" 'firefly_shutdown'
+kill "$kevm_client_pid" || true
 
 exit "$exit_code"
