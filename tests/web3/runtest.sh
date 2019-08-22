9a555a5dafc665dccf69a768c46f8ab56e71c5cd
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-08-22 12:18:37 -0600

    tests/web3/runtest.sh: make sure timeout doesnt force test to fail

--- tests/web3/runtest.sh
+++ tests/web3/runtest.sh
@@ -18,8 +18,8 @@ cat "$input_file" | netcat 127.0.0.1 "$PORT" -q 0 > "$tmp_output_file"
 
 ./kevm web3-send "$PORT" 'firefly_shutdown'
 echo
-timeout 20 tail --pid="$kevm_client_pid" -f /dev/null
-pkill -P "$kevm_client_pid" kevm-client || true
+timeout 20 tail --pid="$kevm_client_pid" -f /dev/null || true
+pkill -P "$kevm_client_pid" kevm-client               || true
 
 exit_code='0'
 git --no-pager diff --no-index "$output_file" "$tmp_output_file" || exit_code="$?"
