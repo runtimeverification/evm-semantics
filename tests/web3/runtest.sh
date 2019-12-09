c76681b924e4515761d4e3e7a7e15ab69adf0219
Andrei <andrei.vacaru@runtimeverification.com>
2019-12-09 18:20:06 +0200

    runtest.sh: tweak curl cmd

--- tests/web3/runtest.sh
+++ tests/web3/runtest.sh
@@ -12,8 +12,7 @@ kevm_client_pid="$!"
 while ! netcat -z 127.0.0.1 "$PORT"; do sleep 0.1; done
 
 # Feed input in, store output in supplied file
-#[TODO]
-curl -i -X POST 127.0.0.1:"$PORT" --data @"$input_file" | jq . > "$output_file"
+curl -s -X POST 127.0.0.1:"$PORT" --data @"$input_file" | jq . > "$output_file"
 
 ./kevm web3-send "$PORT" 'firefly_shutdown'
 echo
