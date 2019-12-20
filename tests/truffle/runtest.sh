da034826390ed6d3f2542bf0408b7b08e0275eeb
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-12-20 01:22:20 -0700

    tests/truffle/runtest: remove debugging stuff

--- tests/truffle/runtest.sh
+++ tests/truffle/runtest.sh
@@ -6,7 +6,7 @@ test_dir="$1" ; shift
 
 # launch test-runner
 PORT=8485
-{ ./kevm web3-ganache --dump "$PORT" --shutdownable &> blah ; } &
+./kevm web3-ganache "$PORT" --shutdownable &
 kevm_client_pid="$!"
 
 while (! netcat -z 127.0.0.1 "$PORT") ; do sleep 0.1; done
