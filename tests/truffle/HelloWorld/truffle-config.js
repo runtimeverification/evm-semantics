c8ddc47a3ab21c5b32b636f5c2921535394e219c
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-12-20 01:22:36 -0700

    tests/truffle/HelloWorld: add simple test

---
+++ tests/truffle/HelloWorld/truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8485,
+            network_id: "*"
+        }
+    },
+    mocha: {
+    },
+    compilers: {
+        solc: {
+        }
+    }
+}
