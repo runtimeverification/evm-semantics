c4b6a1655e37ba599c211b411bf54d04f1405256
Andrei Vacaru <andrei.vacaru@runtimeverification.com>
2019-12-20 11:17:52 +0200

    add package.json

--- tests/truffle/HelloWorld/contracts/HelloWorld.sol
+++ tests/truffle/HelloWorld/contracts/HelloWorld.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.8;
+pragma solidity ^0.5.8;
 
 contract HelloWorld {
     string private message = "hello world";
--- tests/truffle/HelloWorld/truffle-config.js
+++ tests/truffle/HelloWorld/truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
-            port: 8485,
+            port: 8545,
             network_id: "*"
         }
     },
