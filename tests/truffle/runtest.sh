8917c448073bfe28b8b074b0c4b6cf03429d74fb
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-12-19 23:22:53 -0700

    tests/{truffle.test.sh,truffle/runtest.sh}: generic testing harness

---
+++ tests/truffle/runtest.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+test_dir="$1" ; shift
+
+# launch test-runner
+PORT=8485
+{ ./kevm web3-ganache --dump "$PORT" --shutdownable &> blah ; } &
+kevm_client_pid="$!"
+
+while (! netcat -z 127.0.0.1 "$PORT") ; do sleep 0.1; done
+
+pushd "$test_dir"
+truffle test
+popd
+
+# close test-runner
+./kevm web3-send "$PORT" 'firefly_shutdown'
+echo
+timeout 8 tail --pid="$kevm_client_pid" -f /dev/null || true
+pkill -P "$kevm_client_pid" kevm-client              || true
+timeout 8 tail --pid="$kevm_client_pid" -f /dev/null || true
