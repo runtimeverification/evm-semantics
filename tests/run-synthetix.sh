55742ea521289ef7b2d7965bb29f65a16b0ba875
anvacaru <16517508+anvacaru@users.noreply.github.com>
2020-01-28 18:22:31 +0200

    Run Synthetix Tests (#697)

--- tests/run-synthetix.sh
+++ tests/run-synthetix.sh
@@ -14,17 +14,17 @@ kevm_client_pid="$!"
 
 while (! netcat -z 127.0.0.1 "$PORT") ; do sleep 0.1; done
 
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501203", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501204", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501205", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501206", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209", "balance":"0xd3c21bcecceda1000000"}'
-./kevm web3-send "$PORT" firefly_setGasLimit '"0xfffffffffff"'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501203", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501204", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501205", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501206", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_addAccount '{"key":"0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209", "balance":"0x56BC75E2D63100000"}'
+./kevm web3-send "$PORT" firefly_setGasLimit '"0x7A1200"'
 ./kevm web3-send "$PORT" evm_increaseTime '1579773024'
 ./kevm web3-send "$PORT" firefly_genesisBlock
 
