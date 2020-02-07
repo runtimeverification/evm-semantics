12dfd6d60bb1d54e96faed34cacafa93773b4c15
Andrei Vacaru <andrei.vacaru@runtimeverification.com>
2020-02-07 14:54:30 +0200

    fetch function data from a single json file

--- fetchFunctionData.sh
+++ fetchFunctionData.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 folder_path="$1" ; shift
 
-cat "$folder_path"/ERC20.json | jq '[.abi[] | {name:.name, type: .type, inputs:[.inputs[]]}]'
+cat "$folder_path"/ERC20.json | jq '{contractName: .contractName, functionData: [.abi[] | select(.type == "function") | {name: .name, input:[ .inputs[] |{name: .name, type:.type}], output:[.outputs[] |{name: .name, type:.type} ]}]}'
