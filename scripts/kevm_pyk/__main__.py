33cfe330e1ca72c94410ec59c89967ab50093784
Tamas Toth <tamas.toth@runtimeverification.com>
2022-03-29 10:10:16 +0200

    Reorganize __main__.py

--- scripts/kevm_pyk/__main__.py
+++ scripts/kevm_pyk/__main__.py
@@ -4,11 +4,14 @@ import sys
 from .solc_to_k import solc_to_k
 
 
-def main(args):
+def main():
+    sys.setrecursionlimit(15000000)
+    parser = create_argument_parser()
+    args = parser.parse_args()
+
     if args.command == 'solc-to-k':
         res = solc_to_k(**vars(args))
-        sys.stdout.write(res)
-        sys.stdout.flush()
+        print(res)
 
 
 def create_argument_parser():
@@ -16,14 +19,12 @@ def create_argument_parser():
     command_parser = parser.add_subparsers(dest='command')
     solc_command_parser = command_parser.add_parser('solc-to-k', help='Output helper K definition for given JSON output from solc compiler.')
     solc_command_parser.add_argument('kompiled_directory', type=str, help='Path to *-kompiled directory to use.')
-    solc_command_parser.add_argument('contract_file', type=str, help='Combined JSON output from solc compiler.')
+    solc_command_parser.add_argument('contract_file', type=str, help='Path to contract file.')
     solc_command_parser.add_argument('solc_output', type=argparse.FileType('r'), default='-', help='Combined JSON output from solc compiler.')
     solc_command_parser.add_argument('contract_name', type=str, help='Name of contract to generate K helpers for.')
     solc_command_parser.add_argument('--no-storage-slots', dest='generate_storage', default=True, action='store_false', help='Do not generate productions and rules for accessing storage slots')
     return parser
 
 
-sys.setrecursionlimit(15000000)
-parser = create_argument_parser()
-args = parser.parse_args()
-main(args)
+if __name__ == "__main__":
+    main()
