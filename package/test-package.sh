ce599956fea4f97c30be47379d508f96ad78cd6d
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-04-30 04:32:32 +0000

    package/test-package: remove not working tests

--- package/test-package.sh
+++ package/test-package.sh
@@ -12,12 +12,6 @@ kevm run tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTes
     || git --no-pager diff --no-index --ignore-all-space -R tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json.llvm-out tests/templates/output-success-llvm.json
 rm -rf tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json.llvm-out
 
-kevm run tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json --backend haskell \
-    --mode VMTESTS --schedule DEFAULT --chainid 1                                                             \
-    > tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json.haskell-out          \
-    || git --no-pager diff --no-index --ignore-all-space -R tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json.haskell-out tests/templates/output-success-haskell.json
-rm -rf tests/ethereum-tests/LegacyTests/Constantinople/VMTests/vmArithmeticTest/add0.json.haskell-out
-
 kevm kast tests/interactive/log3_MaxTopic_d0g0v0.json kast  --backend llvm > tests/interactive/log3_MaxTopic_d0g0v0.json.parse-out
 git --no-pager diff --no-index --ignore-all-space -R tests/interactive/log3_MaxTopic_d0g0v0.json.parse-out tests/interactive/log3_MaxTopic_d0g0v0.json.parse-expected
 rm -rf tests/interactive/log3_MaxTopic_d0g0v0.json.parse-out
@@ -46,7 +40,3 @@ kevm kompile --backend java tests/specs/erc20/verification.k   \
     --debug
 kevm prove tests/specs/erc20/ds/transfer-failure-1-a-spec.k --backend java --format-failures --debugger \
     --directory tests/specs/erc20/verification/java
-
-kevm search tests/interactive/search/branching-invalid.evm "<statusCode> EVMC_INVALID_INSTRUCTION </statusCode>"  --backend haskell > tests/interactive/search/branching-invalid.evm.search-out
-git --no-pager diff --no-index --ignore-all-space -R tests/interactive/search/branching-invalid.evm.search-out tests/interactive/search/branching-invalid.evm.search-expected
-rm -rf tests/interactive/search/branching-invalid.evm.search-out
