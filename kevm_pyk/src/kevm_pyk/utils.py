c51328e6f74d886e5987debe23e71b723e421978
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-06-06 21:42:06 -0600

    Add pyk runner for prover and kompile, rule profiling (#1258)
    * .gitignore: ignore generated debug logs
    
    * kevm_pyk/, kevm: factor out KEVM class, begin adding prove command
    
    * kevm, kevm_pyk, Makefile: single working proof test with --pyk-prove argument
    
    * .gitignore: ignore debug logs
    
    * kevm_pyk/: default to turning on rule profiling when using --pyk-prove
    
    * .gitignore: ignore generated rule-profile
    
    * kevm_pyk/requirements: tabulate/types-tabulate
    
    * Makefile: add test of quick spec with --pyk-prove
    
    * Makefile: only activate virtualenv for test-kevm-pyk
    
    * kevm_pyk/kevm.py: correct default argument pass through
    
    * kevm_pyk/: move kevm constructors to kevm.py
    
    * kevm: rename argument --pyk-prove => --pyk
    
    * kevm_pyk: add static kompile method to KEVM class which returns a built kevm
    
    * kevm_pyk: add kompile frontend command
    
    * kevm: add --pyk option to kevm kompile as well
    
    * Makefile: enable testing kevm_pyk kompile on CI
    
    * kevm_pyk/solc_to_k: dont included generated cells in proofs
    
    * kevm_pyk, kevm/: support --concrete-rule-files in kevm_pyk
    
    * kevm_pyk/solc_to_k: hardcode pretty-printer for abi_selector
    
    * kevm_pyk/__main__: factor out common arguments into parent parser
    
    * kevm: add --verbose argument for passing through to kevm_pyk
    
    * Makefile: adjust dependencies, add --verbose
    
    * Jenkinsfile: make sure test-kevm-pyk works for all build steps
    
    * Makefile: print out length of profile file after test-kevm-pyk
    
    * Makefile: add kevm-pyk-venv as dependency of test-kevm-pyk
    
    * Jenkinsfile, Makefile: build virtualenv before test-kevm-pyk
    
    * kevm_pyk/: configuration => config
    
    * Jenkinsfile: rename stage

--- kevm_pyk/src/kevm_pyk/utils.py
+++ kevm_pyk/src/kevm_pyk/utils.py
@@ -1,99 +1,15 @@
-from typing import List
-
-from pyk.kast import (
-    KApply,
-    KInner,
-    KNonTerminal,
-    KTerminal,
-    KVariable,
-    flattenLabel,
-)
+from pyk.kast import KApply, KNonTerminal, KTerminal, KVariable
 from pyk.kastManip import (
     abstractTermSafely,
-    getCell,
     isAnonVariable,
     splitConfigAndConstraints,
     splitConfigFrom,
     substitute,
 )
-from pyk.prelude import intToken, stringToken
 
 
-def pow256():
-    return KApply('pow256_EVM-TYPES_Int', [])
-
-
-def rangeUInt160(i: KInner) -> KApply:
-    return KApply('#rangeUInt(_,_)_EVM-TYPES_Bool_Int_Int', [intToken(160), i])
-
-
-def rangeUInt256(i: KInner) -> KApply:
-    return KApply('#rangeUInt(_,_)_EVM-TYPES_Bool_Int_Int', [intToken(256), i])
-
-
-def rangeAddress(i: KInner) -> KApply:
-    return KApply('#rangeAddress(_)_EVM-TYPES_Bool_Int', [i])
-
-
-def rangeBool(i: KInner) -> KApply:
-    return KApply('#rangeBool(_)_EVM-TYPES_Bool_Int', [i])
-
-
-def sizeByteArray(ba: KInner) -> KApply:
-    return KApply('#sizeByteArray(_)_EVM-TYPES_Int_ByteArray', [ba])
-
-
-def infGas(g: KInner) -> KApply:
-    return KApply('infGas', [g])
-
-
-def computeValidJumpDests(p: KInner) -> KApply:
-    return KApply('#computeValidJumpDests(_)_EVM_Set_ByteArray', [p])
-
-
-def binRuntime(c: KInner) -> KApply:
-    return KApply('#binRuntime', [c])
-
-
-def abiCallData(n: str, args: List[KInner]):
-    token: KInner = stringToken(n)
-    return KApply('#abiCallData', [token] + args)
-
-
-def abiAddress(a: KInner) -> KApply:
-    return KApply('#address(_)_EVM-ABI_TypedArg_Int', [a])
-
-
-def abiBool(b: KInner) -> KApply:
-    return KApply('#bool(_)_EVM-ABI_TypedArg_Int', [b])
-
-
-def emptyTypedArgs() -> KApply:
-    return KApply('.List{"_,__EVM-ABI_TypedArgs_TypedArg_TypedArgs"}_TypedArgs')
-
-
-def bytesAppend(b1: KInner, b2: KInner) -> KApply:
-    return KApply('_++__EVM-TYPES_ByteArray_ByteArray_ByteArray', [b1, b2])
-
-
-def kevmAccountCell(
-    id: KInner,
-    balance: KInner,
-    code: KInner,
-    storage: KInner,
-    origStorage: KInner,
-    nonce: KInner,
-) -> KApply:
-    return KApply('<account>', [KApply('<acctID>', [id]),
-                                KApply('<balance>', [balance]),
-                                KApply('<code>', [code]),
-                                KApply('<storage>', [storage]),
-                                KApply('<origStorage>', [origStorage]),
-                                KApply('<nonce>', [nonce])])
-
-
-def kevmWordStackLen(constrainedTerm: KInner) -> int:
-    return len(flattenLabel('_:__EVM-TYPES_WordStack_Int_WordStack', getCell(constrainedTerm, 'WORDSTACK_CELL')))
+def add_include_arg(includes):
+    return [arg for include in includes for arg in ['-I', include]]
 
 
 def abstract_cell_vars(cterm):
@@ -128,7 +44,7 @@ def get_label_for_cell_sorts(definition, sort):
     return productions[0].klabel
 
 
-def build_empty_configuration_cell(definition, sort):
+def build_empty_config_cell(definition, sort):
     label = get_label_for_cell_sorts(definition, sort)
     production = get_production_for_klabel(definition, label)
     args = []
@@ -138,7 +54,7 @@ def build_empty_configuration_cell(definition, sort):
         if type(p_item) is KNonTerminal:
             num_nonterminals += 1
             if p_item.sort.name.endswith('Cell'):
-                args.append(build_empty_configuration_cell(definition, p_item.sort))
+                args.append(build_empty_config_cell(definition, p_item.sort))
             else:
                 num_freshvars += 1
                 args.append(KVariable(sort.name[0:-4].upper() + '_CELL'))
