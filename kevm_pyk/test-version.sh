309d4140f95c8783779203c86aeab3f1434e2633
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-06-12 16:10:41 +0000

    kevm_pyk/test-version.sh: correction

--- kevm_pyk/test-version.sh
+++ kevm_pyk/test-version.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 submodule_version="$1" ; shift
 kevm_pyk_version="$(cat kevm_pyk/setup.cfg | grep 'pyk @ git' | cut --delimiter='@' --field=3 | cut --delimiter='#' --field=1)"
 
-[[ "${submodule_version}" == "${kevm_pyk_version}" ]]
+[[ "v${submodule_version}" == "${kevm_pyk_version}" ]]
