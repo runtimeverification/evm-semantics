b23f4270b24c145360f83259761673360bebf543
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-04 00:29:49 +0000

    kevm_pyk/set-version: make sure tags are fetched

--- kevm_pyk/set-version.sh
+++ kevm_pyk/set-version.sh
@@ -2,7 +2,11 @@
 
 set -euxo pipefail
 
+cd deps/k
+git fetch --tags
 K_COMMIT="$(cd deps/k && git tag --points-at HEAD | cut --characters=2-)"
+cd ../..
+
 cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${K_COMMIT}"'/' > kevm_pyk/setup.cfg
 git add kevm_pyk/setup.cfg
 ! git commit -m "kevm_pyk/setup.cfg: update version to ${K_COMMIT}" || git push
