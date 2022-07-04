3234c4624c60266f1762959853247ffef518fe1c
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-04 00:32:51 +0000

    kevm_pyk/set-version: correct error

--- kevm_pyk/set-version.sh
+++ kevm_pyk/set-version.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 cd deps/k
 git fetch origin 'refs/tags/*:refs/tags/*'
-K_COMMIT="$(cd deps/k && git tag --points-at HEAD | cut --characters=2-)"
+K_COMMIT="$(git tag --points-at HEAD | cut --characters=2-)"
 cd ../..
 
 cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${K_COMMIT}"'/' > kevm_pyk/setup.cfg
