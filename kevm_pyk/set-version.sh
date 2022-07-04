c1acdbe039ecad5921e0b54c8ed8bef543751567
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-04 00:28:05 +0000

    .github/update-version, kevm_pyk/set-version: simplify script

--- kevm_pyk/set-version.sh
+++ kevm_pyk/set-version.sh
@@ -2,5 +2,7 @@
 
 set -euxo pipefail
 
-submodule_version="$1" ; shift
-cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${submodule_version}"'/' > kevm_pyk/setup.cfg
+K_COMMIT="$(cd deps/k && git tag --points-at HEAD | cut --characters=2-)"
+cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${K_COMMIT}"'/' > kevm_pyk/setup.cfg
+git add kevm_pyk/setup.cfg
+! git commit -m "kevm_pyk/setup.cfg: update version to ${K_COMMIT}" || git push
