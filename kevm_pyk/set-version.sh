ec687cbe37965be30f89353d9374e252ce32b5b6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-04 00:41:45 +0000

    .github/update-version, kevm_pyk/set-version: attempt to us checkout@v3 to do the push

--- kevm_pyk/set-version.sh
+++ kevm_pyk/set-version.sh
@@ -9,4 +9,4 @@ cd ../..
 
 cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${K_COMMIT}"'/' > kevm_pyk/setup.cfg
 git add kevm_pyk/setup.cfg
-! git commit -m "kevm_pyk/setup.cfg: update version to ${K_COMMIT}" || git push "HEAD:${GITHUB_REF#refs/heads/}"
+git commit -m "kevm_pyk/setup.cfg: update version to ${K_COMMIT}" || true
