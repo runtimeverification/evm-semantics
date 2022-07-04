73bd20ecd200f1217d71bed321ad4edac008ba97
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-04 00:59:51 +0000

    .github, kevm_pyk: try the add/commit/push in runner

--- kevm_pyk/set-version.sh
+++ kevm_pyk/set-version.sh
@@ -8,5 +8,3 @@ K_COMMIT="$(git tag --points-at HEAD | cut --characters=2-)"
 cd ../..
 
 cat kevm_pyk/setup.cfg.tmpl | sed 's/\${K_COMMIT}/'"${K_COMMIT}"'/' > kevm_pyk/setup.cfg
-git add kevm_pyk/setup.cfg
-git commit -m "kevm_pyk/setup.cfg: update version to ${K_COMMIT}" || true
