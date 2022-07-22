72ecfb198742a9ebcfb0c5e42f3c4fed0e4cdbf4
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-22 02:40:37 +0000

    kevm_pyk/solc_to_k, tests/foundry: only generate specs for test_* functions

--- kevm_pyk/src/kevm_pyk/solc_to_k.py
+++ kevm_pyk/src/kevm_pyk/solc_to_k.py
@@ -140,9 +140,10 @@ def contract_to_k(contract_json: Dict, contract_name: str, generate_storage: boo
     for ftp in function_test_productions:
         klabel = ftp.klabel
         assert isinstance(klabel, KLabel)
-        args = [abstract_term_safely(KVariable('_###SOLIDITY_ARG_VAR###_'), base_name='V') for pi in ftp.items if type(pi) is KNonTerminal]
-        calldata: KInner = KApply(contract_function_application_label, [KApply(contract_klabel), KApply(klabel, args)])
-        function_test_calldatas.append(calldata)
+        if klabel.name.startswith(f'{contract_name}_function_test'):
+            args = [abstract_term_safely(KVariable('_###SOLIDITY_ARG_VAR###_'), base_name='V') for pi in ftp.items if type(pi) is KNonTerminal]
+            calldata: KInner = KApply(contract_function_application_label, [KApply(contract_klabel), KApply(klabel, args)])
+            function_test_calldatas.append(calldata)
     claims = gen_claims_for_contract(empty_config, contract_name, calldata_cells=function_test_calldatas)
     claims_module = KFlatModule(module_name + '-SPEC', claims, [KImport(module_name)]) if function_test_productions else None
 
