7cb1e1f99857c23a702980f487ca99c75cb49b17
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-24 21:47:39 +0000

    tests/foundry: add storage slots

--- tests/foundry/src/Token.sol
+++ tests/foundry/src/Token.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Token {}
+contract Token {
+    uint256 x;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+}
