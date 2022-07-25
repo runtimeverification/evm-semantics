e97c4ad5cfbef309ac7bda69d079f4ab2b6c90da
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-07-25 04:00:41 +0000

    tests/foundry: add example methods

--- tests/foundry/src/Token.sol
+++ tests/foundry/src/Token.sol
@@ -5,4 +5,13 @@ contract Token {
     uint256 x;
     mapping(address => uint256) balances;
     mapping(address => mapping(address => uint256)) allowances;
+
+    function _move(address src, address dst, uint256 amount) internal {
+        balances[src] = balances[src] - amount;
+        balances[dst] = balances[dst] + amount;
+    }
+
+    function transfer(address dst, uint256 amount) external {
+        _move(msg.sender, dst, amount);
+    }
 }
