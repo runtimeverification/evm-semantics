61d992b14db832ac9c3fa681f3daca0b4214e27f
lisandrasilva <lisandrasilva18@hotmail.com>
2022-07-28 10:27:26 +0100

    Fixed some tests

--- tests/foundry/test/FilesTest.t.sol
+++ tests/foundry/test/FilesTest.t.sol
@@ -6,26 +6,26 @@ import "forge-std/Test.sol";
 contract FilesTest is Test {
     
     function testReadWriteFile() public {
-        string memory contents = vm.readFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/file.txt");
-        vm.writeFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/filecopy.txt", contents);
-        string memory contentsCopy = vm.readFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/filecopy.txt");
+        string memory contents = vm.readFile("./test/file.txt");
+        vm.writeFile("./test/filecopy.txt", contents);
+        string memory contentsCopy = vm.readFile("./test/filecopy.txt");
 
         assertEq(contents, contentsCopy);
     }
 
     function testReadWriteLine() public {
-        string memory line = vm.readLine("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/file.txt");
-        vm.writeLine("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/filecopy.txt", line);
-        line = vm.readLine("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/file.txt");
+        string memory line = vm.readLine("./test/file.txt");
+        vm.writeLine("./test/filecopy.txt", line);
+        line = vm.readLine("./test/file.txt");
         assertEq(line, "for testing");
         
-        vm.closeFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/file.txt");
-        line = vm.readLine("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/file.txt");
+        vm.closeFile("./test/file.txt");
+        line = vm.readLine("./test/file.txt");
         assertEq(line, "This is a file");
     }
 
     function testFailRemoveFile() public {
-        vm.removeFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/filecopy.txt");
-        vm.readFile("/Users/lisandrasilva/evm-semantics/tests/gen-spec/foundry/test/filecopy.txt");
+        vm.removeFile("./test/filecopy.txt");
+        vm.readFile("./test/filecopy.txt");
     }
 }
