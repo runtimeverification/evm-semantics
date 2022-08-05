21ec1e8fad5e1f47cbcd435c611fdbda15e8fd19
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-08-05 15:22:58 +0000

    tests/foundry: working tests

--- tests/foundry/test/FilesTest.t.sol
+++ tests/foundry/test/FilesTest.t.sol
@@ -4,28 +4,30 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract FilesTest is Test {
-    
-//    function testReadWriteFile() public {
-//        string memory contents = vm.readFile("./test/file.txt");
-//        vm.writeFile("./test/filecopy.txt", contents);
-//        string memory contentsCopy = vm.readFile("./test/filecopy.txt");
-//
-//        assertEq(contents, contentsCopy);
-//    }
+
+    function testReadWriteFile() public {
+        string memory contents = vm.readFile("./test/file.txt");
+        vm.writeFile("./test/filecopy.txt", contents);
+        string memory contentsCopy = vm.readFile("./test/filecopy.txt");
+
+        assertEq(contents, contentsCopy);
+    }
 
     function testReadWriteLine() public {
         string memory line = vm.readLine("./test/file.txt");
-        vm.writeLine("./test/filecopy.txt", line);
+        vm.writeLine("./test/fileline.txt", line);
         line = vm.readLine("./test/file.txt");
         assertEq(line, "for testing");
-        
+
         vm.closeFile("./test/file.txt");
         line = vm.readLine("./test/file.txt");
         assertEq(line, "This is a file");
     }
 
     function testFailRemoveFile() public {
-        vm.removeFile("./test/filecopy.txt");
-        vm.readFile("./test/filecopy.txt");
+        string memory contents = vm.readFile("./test/file.txt");
+        vm.writeFile("./test/filecopy2.txt", contents);
+        vm.removeFile("./test/filecopy2.txt");
+        vm.readFile("./test/filecopy2.txt");
     }
 }
