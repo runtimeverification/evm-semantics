f513c1eb799aaa66a0d8d438da2efe59695edec9
RV Jenkins <admin@runtimeverification.com>
2022-08-28 15:03:00 -0500

    gh-pages: Updated the website

--- tests/failing-symbolic.java
+++
@@ -1,7 +0,0 @@
-tests/specs/examples/erc20-spec.md
-tests/specs/examples/erc721-spec.md
-tests/specs/examples/storage-spec.md
-tests/specs/functional/infinite-gas-spec.k
-tests/specs/functional/merkle-spec.k
-tests/specs/functional/storageRoot-spec.k
-tests/specs/opcodes/evm-optimizations-spec.md
