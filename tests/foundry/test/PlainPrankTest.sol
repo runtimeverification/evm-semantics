f7943c5478a6a0c518650c6e479d0b78df774073
Andrei <andrei.vacaru@runtimeverification.com>
2022-09-02 13:40:51 +0300

    add new tests for startPrank

---
+++ tests/foundry/test/PlainPrankTest.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.13;
+
+import "forge-std/Test.sol";
+import "src/Prank.sol";
+
+contract AdditionalToken {
+    address public immutable owner;
+    uint256 public count;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function incrementCount() public{
+        require(msg.sender != owner);
+        count = count + 1;
+    }
+}
+
+contract PlainPrankTest is Test {
+    function test_startPrank_true() public {
+        AdditionalToken token = new AdditionalToken();
+        vm.startPrank(address(token));
+        token.incrementCount();
+        vm.stopPrank();
+        assert(token.count() == 1);
+    }
+}
