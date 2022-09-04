16f8adf6368394c34648101b289af6a339ab29bf
Andrei <andrei.vacaru@runtimeverification.com>
2022-09-04 10:33:40 +0300

    add more tests for startPrank/stopPrank

--- tests/foundry/test/PlainPrankTest.t.sol
+++ tests/foundry/test/PlainPrankTest.t.sol
@@ -20,11 +20,43 @@ contract AdditionalToken {
 }
 
 contract PlainPrankTest is Test {
+
     function test_startPrank_true() public {
         AdditionalToken token = new AdditionalToken();
         vm.startPrank(address(token));
         token.incrementCount();
         vm.stopPrank();
         assert(token.count() == 1);
+    }
+
+    function test_startPrankWithOrigin_true() public {
+        AdditionalToken token = new AdditionalToken();
+        vm.startPrank(address(token), address(token));
+        token.incrementCount();
+        vm.stopPrank();
+        assert(token.count() == 1);
+    }
+
+    function test_stopPrank_true() public {
+        vm.stopPrank();
+    }
+
+    function test_stopPrank_true2() public {
+        AdditionalToken token = new AdditionalToken();
+        vm.startPrank(address(token));
+        token.incrementCount();
+        assert(token.count() == 1);
+    }
+
+    function testFail_startPrank_false() public {
+        AdditionalToken token = new AdditionalToken();
+        vm.startPrank(address(token));
+        AdditionalToken token2 = new AdditionalToken();
+        assert(token2.owner() == address(token));
+        vm.startPrank(address(token2));
+        AdditionalToken token3 = new AdditionalToken();
+        assert(token3.owner() == address(token2));
+        vm.stopPrank();
+        vm.stopPrank();
     }
 }
