46151fa5372a69a9b83d3befb65e83457b6c5fd1
lisandrasilva <lisandrasilva18@hotmail.com>
2022-09-06 14:03:22 +0100

    Updated tests

--- tests/foundry/test/AddrTest.t.sol
+++ tests/foundry/test/AddrTest.t.sol
@@ -10,16 +10,16 @@ contract AddrTest is Test {
         assertEq(alice, 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
     }
 
-    //function test_addr_false() public {
-    //    address alice = vm.addr(0);
-    //}
+    function test_addr_false() public {
+        address alice = vm.addr(0);
+    }
 
     function testFail_addr_true() public {
         address alice = vm.addr(115792089237316195423570985008687907852837564279074904382605163141518161494337);
     }
 
-    //function testFail_addr_false() public {
-    //    address alice = vm.addr(1);
-    //    assertEq(alice, 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
-    //}
+    function testFail_addr_false() public {
+        address alice = vm.addr(1);
+        assertEq(alice, 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
+    }
 }
