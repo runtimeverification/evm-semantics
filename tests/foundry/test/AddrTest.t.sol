ee21573c8ab0105e60d98c19653630389d9afbf0
lisandrasilva <lisandrasilva18@hotmail.com>
2022-09-06 13:46:31 +0100

    Updated tests for addr cheatcode

--- tests/foundry/test/AddrTest.t.sol
+++ tests/foundry/test/AddrTest.t.sol
@@ -5,29 +5,21 @@ import "forge-std/Test.sol";
 
 contract AddrTest is Test {
 
-    function testAddr() public {
+    function test_addr_true() public {
         address alice = vm.addr(1);
         assertEq(alice, 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
     }
 
-    function test_addr_true() public {
-        address alice = vm.addr(1);
-        assert(alice == 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
-    }
-
-    function test_addr_false() public {
-        address alice = vm.addr(1);
-        assert(alice != 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
-    }
+    //function test_addr_false() public {
+    //    address alice = vm.addr(0);
+    //}
 
     function testFail_addr_true() public {
-        address alice = vm.addr(1);
-        assert(alice != 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
+        address alice = vm.addr(115792089237316195423570985008687907852837564279074904382605163141518161494337);
     }
 
-    function testFail_addr_false() public {
-        address alice = vm.addr(1);
-        assert(alice == 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
-    }
-
+    //function testFail_addr_false() public {
+    //    address alice = vm.addr(1);
+    //    assertEq(alice, 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf);
+    //}
 }
