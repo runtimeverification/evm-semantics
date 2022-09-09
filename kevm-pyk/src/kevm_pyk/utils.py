3b66c908855be4df16319c37fbb7bb22d70d3db6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-09-09 19:31:46 +0000

    kevm-pyk/utils.py: bring in KCFG_from_claim from downstream

--- kevm-pyk/src/kevm_pyk/utils.py
+++ kevm-pyk/src/kevm_pyk/utils.py
@@ -1,8 +1,42 @@
 from typing import Collection, Iterable, List
 
-from pyk.kast import KDefinition, KFlatModule, KImport, KTerminal, KVariable
-from pyk.kastManip import abstract_term_safely, is_anon_var, split_config_and_constraints, split_config_from, substitute
+from pyk.cfg_manager import instantiate_cell_vars, rename_generated_vars
+from pyk.cterm import CTerm
+from pyk.kast import KClaim, KDefinition, KFlatModule, KImport, KTerminal, KVariable
+from pyk.kastManip import (
+    abstract_term_safely,
+    bool_to_ml_pred,
+    extract_lhs,
+    extract_rhs,
+    is_anon_var,
+    split_config_and_constraints,
+    split_config_from,
+    substitute,
+)
+from pyk.kcfg import KCFG
 from pyk.ktool import KPrint
+from pyk.prelude import Bool, mlAnd
+
+
+def KCFG_from_claim(defn: KDefinition, claim: KClaim) -> KCFG:  # noqa: N802
+    cfg = KCFG()
+    claim_body = claim.body
+    claim_body = instantiate_cell_vars(defn, claim_body)
+    claim_body = rename_generated_vars(CTerm(claim_body)).kast
+
+    claim_lhs = extract_lhs(claim_body)
+    claim_lhs = claim_lhs if claim.requires == Bool.true else mlAnd([claim_lhs, bool_to_ml_pred(claim.requires)])
+    claim_lhs_cterm = CTerm(claim_lhs)
+    init_state = cfg.create_node(claim_lhs_cterm)
+    cfg.add_init(init_state.id)
+
+    claim_rhs = extract_rhs(claim_body)
+    claim_rhs = claim_rhs if claim.ensures == Bool.true else mlAnd([claim_rhs, bool_to_ml_pred(claim.ensures)])
+    claim_rhs_cterm = CTerm(claim_rhs)
+    target_state = cfg.create_node(claim_rhs_cterm)
+    cfg.add_target(target_state.id)
+
+    return cfg
 
 
 def KPrint_make_unparsing(_self: KPrint, extra_modules: Iterable[KFlatModule] = ()) -> KPrint:  # noqa: N802
