d54a98a0e1bddd1ac531ced92712813bd1ecbe26
lisandrasilva <lisandrasilva18@hotmail.com>
2022-09-09 10:11:13 +0100

    Updated tests

--- tests/foundry/test/NonceTest.t.sol
+++ tests/foundry/test/NonceTest.t.sol
@@ -15,37 +15,37 @@ contract NonceTest is Test {
     }
 
     function test_GetNonce_true() public {
-       uint64 oldnonce = vm.getNonce(address(this));
-       assertGt(oldnonce,0);
+       uint64 nonce = vm.getNonce(address(this));
+       assertGt(nonce,0);
     }
 
     function test_GetNonce_false() public {
-       uint64 oldnonce = vm.getNonce(address(this));
-       assert(oldnonce < 0);
+       uint64 nonce = vm.getNonce(address(100));
+       assertEq(nonce, 10);
     }
 
     function testFail_GetNonce_true() public {
-       uint64 oldnonce = vm.getNonce(address(this));
-       assert(oldnonce < 0);
+       uint64 nonce = vm.getNonce(address(0));
+       assertEq(nonce, 10);
     }
 
     function testFail_GetNonce_false() public {
-       uint64 oldnonce = vm.getNonce(address(this));
-       assert(oldnonce >= 0);
+       uint64 nonce = vm.getNonce(address(this));
+       assertGt(nonce, 0);
     }
 
 /*
     function test_SetNonce() public {
        uint64 oldnonce = vm.getNonce(address(this));
-       console.log(oldnonce);
        vm.setNonce(address(this), oldnonce+1);
        assertEq(vm.getNonce(address(this)),oldnonce+1);
     }
     */
 
-    function test_SetNonce_true() public {
-       vm.setNonce(address(this), 100);
-       assertEq(vm.getNonce(address(this)),100);
+    function test_SetNonce() public {
+       vm.setNonce(address(100), 100);
+       uint64 nonce = vm.getNonce(address(100));
+       assert(nonce == 100);
     }
 
 }
