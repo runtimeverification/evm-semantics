bd0a056aebf6c031e902c9feb4cd99f7bf245017
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-09-09 19:34:50 +0000

    kevm-pyk/utils.py: remove unused methods

--- kevm-pyk/src/kevm_pyk/utils.py
+++ kevm-pyk/src/kevm_pyk/utils.py
@@ -2,7 +2,7 @@ from typing import Collection, Iterable, List
 
 from pyk.cfg_manager import instantiate_cell_vars, rename_generated_vars
 from pyk.cterm import CTerm
-from pyk.kast import KClaim, KDefinition, KFlatModule, KImport, KTerminal, KVariable
+from pyk.kast import KClaim, KDefinition, KFlatModule, KImport, KVariable
 from pyk.kastManip import (
     abstract_term_safely,
     bool_to_ml_pred,
@@ -60,30 +60,3 @@ def abstract_cell_vars(cterm, keep_vars: Collection[KVariable] = ()):
         if type(subst[s]) is KVariable and not is_anon_var(subst[s]) and subst[s] not in keep_vars:
             subst[s] = abstract_term_safely(KVariable('_'), base_name=s)
     return substitute(config, subst)
-
-
-def get_productions(definition):
-    return [prod for module in definition.modules for prod in module.productions]
-
-
-def get_production_for_klabel(definition, klabel):
-    productions = [prod for prod in get_productions(definition) if prod.klabel and prod.klabel == klabel]
-    if len(productions) != 1:
-        raise ValueError(f'Expected 1 production for label {klabel}, not {productions}.')
-    return productions[0]
-
-
-def get_label_for_cell_sorts(definition, sort):
-    productions = []
-    for production in get_productions(definition):
-        if production.sort == sort and len(production.items) >= 2:
-            first_arg = production.items[0]
-            if type(first_arg) is KTerminal and not (
-                first_arg.value.startswith('project:')
-                or first_arg.value.startswith('init')
-                or first_arg.value.startswith('get')
-            ):
-                productions.append(production)
-    if len(productions) != 1:
-        raise ValueError(f'Expected 1 production for sort {sort}, not {productions}!')
-    return productions[0].klabel
