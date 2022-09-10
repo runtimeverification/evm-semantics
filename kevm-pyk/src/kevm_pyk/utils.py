45d27c6deb1176e474d0ac80579f01d43c257056
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-09-10 02:14:41 +0000

    kevm-pyk/utils: bring in sanitize_config, refactor from_claim a bit

--- kevm-pyk/src/kevm_pyk/utils.py
+++ kevm-pyk/src/kevm_pyk/utils.py
@@ -3,16 +3,22 @@ from typing import Collection, Iterable, List
 
 from pyk.cfg_manager import instantiate_cell_vars, rename_generated_vars
 from pyk.cterm import CTerm
-from pyk.kast import KClaim, KDefinition, KFlatModule, KFlatModuleList, KImport, KVariable, read_kast
+from pyk.kast import KApply, KClaim, KDefinition, KFlatModule, KFlatModuleList, KImport, KInner, KVariable, read_kast
 from pyk.kastManip import (
     abstract_term_safely,
     bool_to_ml_pred,
+    bottom_up,
     extract_lhs,
     extract_rhs,
+    flatten_label,
+    free_vars,
     is_anon_var,
+    ml_pred_to_bool,
+    remove_generated_cells,
     split_config_and_constraints,
     split_config_from,
     substitute,
+    undo_aliases,
 )
 from pyk.kcfg import KCFG
 from pyk.ktool import KPrint
@@ -26,22 +32,23 @@ def read_kast_flatmodulelist(ifile: Path) -> KFlatModuleList:
 
 
 def KCFG_from_claim(defn: KDefinition, claim: KClaim) -> KCFG:  # noqa: N802
+    def _make_cterm(_kinner: KInner, _cond: KInner) -> CTerm:
+        _kinner = _kinner if _cond == Bool.true else mlAnd([_kinner, bool_to_ml_pred(_cond)])
+        _kinner = sanitize_config(defn, _kinner)
+        return CTerm(_kinner)
+
     cfg = KCFG()
     claim_body = claim.body
     claim_body = instantiate_cell_vars(defn, claim_body)
     claim_body = rename_generated_vars(CTerm(claim_body)).kast
 
-    claim_lhs = extract_lhs(claim_body)
-    claim_lhs = claim_lhs if claim.requires == Bool.true else mlAnd([claim_lhs, bool_to_ml_pred(claim.requires)])
-    claim_lhs_cterm = CTerm(claim_lhs)
-    init_state = cfg.create_node(claim_lhs_cterm)
-    cfg.add_init(init_state.id)
+    claim_lhs = _make_cterm(extract_lhs(claim_body), claim.requires)
+    init_node = cfg.create_node(claim_lhs)
+    cfg.add_init(init_node.id)
 
-    claim_rhs = extract_rhs(claim_body)
-    claim_rhs = claim_rhs if claim.ensures == Bool.true else mlAnd([claim_rhs, bool_to_ml_pred(claim.ensures)])
-    claim_rhs_cterm = CTerm(claim_rhs)
-    target_state = cfg.create_node(claim_rhs_cterm)
-    cfg.add_target(target_state.id)
+    claim_rhs = _make_cterm(extract_rhs(claim_body), claim.ensures)
+    target_node = cfg.create_node(claim_rhs)
+    cfg.add_target(target_node.id)
 
     return cfg
 
@@ -67,3 +74,35 @@ def abstract_cell_vars(cterm, keep_vars: Collection[KVariable] = ()):
         if type(subst[s]) is KVariable and not is_anon_var(subst[s]) and subst[s] not in keep_vars:
             subst[s] = abstract_term_safely(KVariable('_'), base_name=s)
     return substitute(config, subst)
+
+
+def sanitize_config(defn: KDefinition, init_term: KInner) -> KInner:
+    def _var_name(vname):
+        new_vname = vname
+        while new_vname.startswith('_') or new_vname.startswith('?'):
+            new_vname = new_vname[1:]
+        return new_vname
+
+    free_vars_subst = {vname: KVariable(_var_name(vname)) for vname in free_vars(init_term)}
+
+    # TODO: This is somewhat hacky. We shouldn't have to touch the config this much.
+    # Likely, the frontend should just be giving us initial states with these already in place.
+    def _remove_cell_map_definedness(_kast):
+        if type(_kast) is KApply:
+            if _kast.label.name.endswith('CellMap:in_keys'):
+                return Bool.false
+            elif _kast.label.name.endswith('CellMapItem'):
+                return _kast.args[1]
+        return _kast
+
+    new_term = substitute(init_term, free_vars_subst)
+    new_term = remove_generated_cells(new_term)
+    new_term = bottom_up(_remove_cell_map_definedness, new_term)
+
+    if not (type(new_term) is KApply and new_term.label.name in ['#Top', '#Bottom']):
+        config, constraint = split_config_and_constraints(new_term)
+        constraints = [bool_to_ml_pred(ml_pred_to_bool(c, unsafe=True)) for c in flatten_label('#And', constraint)]
+        new_term = mlAnd([config] + constraints)
+        new_term = undo_aliases(defn, new_term)
+
+    return new_term
