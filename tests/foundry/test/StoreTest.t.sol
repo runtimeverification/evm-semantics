0506cd41aac7edb50bb8e575cff47bf78e9c773a
David Kretzmer <david.k@posteo.de>
2022-09-12 17:10:17 +0200

    Support Foundry load/store cheatcodes

--- tests/foundry/test/StoreTest.t.sol
+++ tests/foundry/test/StoreTest.t.sol
@@ -12,22 +12,31 @@ contract Store {
 
 contract StoreTest is Test {
 
-    Store myStore;
-
-    function setUp() public {
-        vm.record();
-        myStore = new Store();
-    }
-
     function testAccesses() public {
+		Store myStore= new Store();
+        vm.record();
+
         (bytes32[] memory reads, bytes32[] memory writes) = vm.accesses(address(myStore));
         assertEq(reads.length, 1);
         assertEq(writes.length, 1);
     }
 
     function testStoreLoad() public {
+		Store myStore= new Store();
+
         vm.store(address(myStore), bytes32(uint256(0)), bytes32(uint256(31337)));
         bytes32 testNumber = vm.load(address(myStore), bytes32(uint256(0)));
         assertEq(uint256(testNumber), 31337); // 31337
+    }
+
+    function testStoreLoadNonExistent() public {
+        vm.store(address(0), bytes32(uint256(3)), bytes32(uint256(31337)));
+        bytes32 testNumber = vm.load(address(0), bytes32(uint256(3)));
+        assertEq(uint256(testNumber), 31337); // 31337
+    }
+
+    function testLoadNonExistent() public {
+        bytes32 testNumber = vm.load(address(100), bytes32(uint256(23)));
+        assertEq(uint256(testNumber), 0);
     }
 }
