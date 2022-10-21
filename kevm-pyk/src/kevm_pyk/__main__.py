af988f64594b5e8dbc2fa65f7c1a77d8f92cc14c
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-10-21 06:14:26 +0000

    kevm-pyk/__main__: add basic auto-exploration strategy for KCFGs

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -9,26 +9,20 @@ from pathos.pools import ProcessPool  # type: ignore
 from pyk.cli_utils import dir_path, file_path
 from pyk.cterm import CTerm, build_rule
 from pyk.kast.inner import KApply, KAtt, KInner, KRewrite, KToken
-from pyk.kast.manip import flatten_label, get_cell, minimize_term, push_down_rewrites
+from pyk.kast.manip import get_cell, minimize_term, push_down_rewrites
 from pyk.kast.outer import KDefinition, KFlatModule, KImport, KRequire, KRule
 from pyk.kcfg import KCFG
 from pyk.kcfg_viewer.app import KCFGViewer
 from pyk.ktool.kit import KIT
 from pyk.ktool.krun import KRunOutput, _krun
 from pyk.prelude.k import GENERATED_TOP_CELL
-from pyk.prelude.ml import is_top, mlTop
+from pyk.prelude.ml import mlTop
+from pyk.utils import shorten_hashes
 
 from .gst_to_kore import gst_to_kore
 from .kevm import KEVM, Foundry
 from .solc_to_k import Contract, contract_to_main_module, method_to_cfg, solc_compile
-from .utils import (
-    KCFG__replace_node,
-    KDefinition__expand_macros,
-    KPrint_make_unparsing,
-    KProve_prove_claim,
-    add_include_arg,
-    sanitize_config,
-)
+from .utils import KCFG__replace_node, KDefinition__expand_macros, KPrint_make_unparsing, add_include_arg
 
 T = TypeVar('T')
 
@@ -286,7 +280,7 @@ def exec_foundry_prove(
     includes: List[str],
     debug_equations: List[str],
     bug_report: bool,
-    depth: Optional[int],
+    max_depth: int,
     reinit: bool = False,
     tests: Iterable[str] = (),
     exclude_tests: Iterable[str] = (),
@@ -376,34 +370,61 @@ def exec_foundry_prove(
 
     lemma_rules = [KRule(KToken(lr, 'K'), att=KAtt({'simplification': ''})) for lr in lemmas]
 
-    def prove_it(_id_and_cfg: Tuple[str, Tuple[KCFG, Path]]) -> bool:
-        _cfg_id, (_cfg, _cfg_path) = _id_and_cfg
-        if len(_cfg.frontier) == 0:
+    def prove_it(id_and_cfg: Tuple[str, Tuple[KCFG, Path]]) -> bool:
+        cfgid, (cfg, cfgpath) = id_and_cfg
+        target_node = cfg.get_unique_target()
+
+        while cfg.frontier:
+            curr_node = cfg.frontier[0]
+            _LOGGER.info(f'Advancing proof from node: {shorten_hashes(curr_node.id)}')
+            edge = KCFG.Edge(curr_node, target_node, mlTop(), -1)
+            claim = edge.to_claim()
+            claim_id = f'gen-{curr_node.id}-to-{target_node.id}'
+            depth, branching, result = foundry.get_claim_basic_block(
+                claim_id, claim, lemmas=lemma_rules, max_depth=max_depth
+            )
+            cfg.add_expanded(curr_node.id)
+
+            if result == mlTop():
+                cfg.create_edge(curr_node.id, target_node.id, mlTop(), depth)
+                _LOGGER.info(
+                    f'Target state reached at depth {depth}, inserted edge from {shorten_hashes((curr_node.id))} to {shorten_hashes((target_node.id))}.'
+                )
+
+            next_state = CTerm(result)
+            next_node = cfg.get_or_create_node(next_state)
+            if next_node != curr_node:
+                _LOGGER.info(f'Found basic block at depth {depth}: {shorten_hashes((curr_node.id, next_node.id))}.')
+                cfg.create_edge(curr_node.id, next_node.id, mlTop(), depth)
+
+            elif KEVM.is_terminal(next_node.cterm):
+                cfg.add_expanded(next_node.id)
+                _LOGGER.info(f'Terminal node: {shorten_hashes((curr_node.id))}.')
+
+            elif branching:
+                branches = KEVM.extract_branches(next_state)
+                if not branches:
+                    raise ValueError(
+                        f'Could not extract branch condition:\n{foundry.pretty_print(minimize_term(result))}'
+                    )
+                cfg.add_expanded(next_node.id)
+                _LOGGER.info(
+                    f'Found {len(list(branches))} branches at depth {depth}: {[foundry.pretty_print(b) for b in branches]}'
+                )
+                for branch in branches:
+                    branch_cterm = next_state.add_constraint(branch)
+                    branch_node = cfg.get_or_create_node(branch_cterm)
+                    cfg.create_cover(branch_node.id, next_node.id)
+
+            write_cfg(cfg, cfgpath)
+
+        failure_nodes = cfg.frontier + cfg.stuck
+        if len(failure_nodes) == 0:
+            _LOGGER.info(f'Proof passed: {cfgid}')
             return True
-        _init_node = _cfg.frontier[0]
-        _target_node = _cfg.get_unique_target()
-        _claim = KCFG.Edge(_init_node, _target_node, mlTop(), -1).to_claim()
-        _claim_id = _cfg_id.replace('.', '-').replace('_', '-')
-        _, result = KProve_prove_claim(foundry, _claim, _claim_id, _LOGGER, depth=depth, lemmas=lemma_rules)
-        if not is_top(result) and retry:
-            _LOGGER.warning(f'Retrying proof once: {_cfg_id}')
-            _, result = KProve_prove_claim(foundry, _claim, _claim_id, _LOGGER, depth=depth, lemmas=lemma_rules)
-        _cfg.add_expanded(_init_node.id)
-        if is_top(result):
-            _cfg.create_edge(_cfg.get_unique_init().id, _cfg.get_unique_target().id, mlTop(), -1)
-            _LOGGER.info(f'Proof passed: {_cfg_id}')
         else:
-            _cfg.add_expanded(_init_node.id)
-            for result_state in flatten_label('#Or', result):
-                result_state = sanitize_config(foundry.definition, result_state)
-                new_node = _cfg.get_or_create_node(CTerm(result_state))
-                _cfg.create_edge(_cfg.get_unique_init().id, new_node.id, mlTop(), -1)
-                if minimize:
-                    result_state = minimize_term(result_state)
-                _LOGGER.error(f'Proof failed: {_cfg_id}\n{foundry.pretty_print(result_state)}')
-        write_cfg(_cfg, _cfg_path)
-        failure_nodes = _cfg.frontier + _cfg.stuck
-        return len(failure_nodes) == 0
+            _LOGGER.error(f'Proof failed: {cfgid}')
+            return False
 
     with ProcessPool(ncpus=workers) as process_pool:
         foundry.close_kore_rpc()
@@ -802,6 +823,13 @@ def _create_argument_parser() -> ArgumentParser:
         default=False,
         action='store_true',
         help='Retry failing proofs once.',
+    )
+    foundry_prove_args.add_argument(
+        '--max-depth',
+        dest='max_depth',
+        default=250,
+        type=int,
+        help='Store every Nth state in the KCFG for inspection.',
     )
 
     foundry_show_args = command_parser.add_parser(
