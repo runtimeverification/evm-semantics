dabf3d701e097eeb13ac59ade92ceb087bbe9744
Andrei <andrei.vacaru@runtimeverification.com>
2022-11-10 15:59:15 +0200

    add test

--- tests/foundry/test/SymbolicStorageTest.t.sol
+++ tests/foundry/test/SymbolicStorageTest.t.sol
@@ -2,13 +2,17 @@
 pragma solidity =0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Vm2.sol";
 
 contract SymbolicStorage is Test { 
+    address constant private VM_ADDRESS =
+        address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
+    Vm2 public constant vm2 = Vm2(VM_ADDRESS);
 
-    /*function testFail_symbolicStorage(uint256 slot) public {
+    function test_symbolicStorage() public {
         address addr = 0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8;
-        //setSymbolicStorage(addr);
-        uint256 value = vm.load(addr, slot);
-        assertEq(value, 0);
-    }*/
+        vm2.symbolicStorage(addr);
+        bytes32 value = vm2.load(addr, bytes32(uint256(0)));
+        assertEq(uint256(value), 0);
+    }
 }
