2571f863f0e03c58e355326669e72bcb78f0a446
lisandrasilva <lisandrasilva18@hotmail.com>
2022-11-11 14:32:40 +0000

    Updated exclude file

--- tests/foundry/test/SymbolicStorageTest.t.sol
+++ tests/foundry/test/SymbolicStorageTest.t.sol
@@ -9,7 +9,7 @@ contract SymbolicStorage is Test {
         address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
     Vm2 public constant vm2 = Vm2(VM_ADDRESS);
 
-    function test_symbolicStorage() public {
+    function testSymbolicStorage() public {
         address addr = 0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8;
         vm2.symbolicStorage(addr);
         bytes32 value = vm2.load(addr, bytes32(uint256(0)));
