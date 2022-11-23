c2c92b72f6a4e46960ac7b16903a54cab4a138fe
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-11-23 06:54:32 +0000

    kevm-pyk/__main__: pass in learned witness instead of inferring it

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -396,7 +396,7 @@ def exec_foundry_prove(
             subsumption = foundry.implies(curr_node.cterm, target_node.cterm)
             if subsumption is not None:
                 final_subst, final_pred = subsumption
-                cfg.create_cover(curr_node.id, target_node.id)
+                cfg.create_cover(curr_node.id, target_node.id, subst=final_subst, constraint=final_pred)
                 _LOGGER.info(f'Subsumed into target node: {shorten_hashes((curr_node.id, target_node.id))}')
                 continue
 
