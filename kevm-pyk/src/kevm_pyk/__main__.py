d684f3c95ba3a948ee0beaf8a4a76dc578dbc6c6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-12-04 19:26:58 +0000

    kevm-pyk/__main__: factor out _write_cfg

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -342,11 +342,6 @@ def exec_foundry_prove(
     # TODO: We should be using lemma_rules for execute endpoint
     # lemma_rules = [KRule(KToken(lr, 'K'), att=KAtt({'simplification': ''})) for lr in lemmas]
 
-    def _write_cfg(_cfg: KCFG, _cfgpath: Path) -> None:
-        with open(_cfgpath, 'w') as cfgfile:
-            cfgfile.write(json.dumps(_cfg.to_dict()))
-            _LOGGER.info(f'Updated CFG file: {_cfgpath}')
-
     kcfgs: Dict[str, Tuple[KCFG, Path]] = {}
     for test in tests:
         kcfg_file = kcfgs_dir / f'{test}.json'
@@ -371,7 +366,7 @@ def exec_foundry_prove(
             cfg, _ = KCFG__replace_node(cfg, cfg.get_unique_init().id, CTerm(init_term))
             cfg, _ = KCFG__replace_node(cfg, cfg.get_unique_target().id, CTerm(target_term))
             kcfgs[test] = (cfg, kcfg_file)
-            _write_cfg(cfg, kcfg_file)
+            write_cfg(cfg, kcfg_file)
         else:
             with open(kcfg_file, 'r') as kf:
                 kcfgs[test] = (KCFG.from_dict(json.loads(kf.read())), kcfg_file)
