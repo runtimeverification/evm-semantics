04c894c10e1e6ff3b3dbd3f4bda2d7d844f26425
Andrei <andrei.vacaru@runtimeverification.com>
2022-12-15 23:20:07 +0200

    refactor Vm2.sol

--- tests/foundry/src/KEVMCheats.sol
+++ tests/foundry/src/KEVMCheats.sol
@@ -2,11 +2,27 @@
 pragma solidity >=0.6.2 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import {Vm2} from "./Vm2.sol";
+interface KEVMCheatsBase {
+    // Expects a call using the CALL opcode to an address with the specified calldata.
+    function expectRegularCall(address,bytes calldata) external;
+    // Expects a call using the CALL opcode to an address with the specified msg.value and calldata.
+    function expectRegularCall(address,uint256,bytes calldata) external;
+    // Expects a static call to an address with the specified calldata.
+    function expectStaticCall(address,bytes calldata) external;
+    // Expects a delegate call to an address with the specified calldata.
+    function expectDelegateCall(address,bytes calldata) external;
+    // Expects that no contract calls are made after invoking the cheatcode.
+    function expectNoCall() external;
+    // Expects the given address to deploy a new contract, using the CREATE opcode, with the specified value and bytecode.
+    function expectCreate(address,uint256,bytes calldata) external;
+    // Expects the given address to deploy a new contract, using the CREATE2 opcode, with the specified value and bytecode (appended with a bytes32 salt).
+    function expectCreate2(address,uint256,bytes calldata) external;
+    // Makes the storage of the given address completely symbolic.
+    function symbolicStorage(address) external;
+}
 
 abstract contract KEVMCheats {
-
-    Vm2 public constant kevm = Vm2(address(uint160(uint256(keccak256("hevm cheat code")))));
+    KEVMCheatsBase public constant kevm = KEVMCheatsBase(address(uint160(uint256(keccak256("hevm cheat code")))));
 
     // Checks if an address matches one of the built-in addresses.
     function notBuiltinAddress(address addr) internal pure returns (bool) {
