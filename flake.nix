0759261d3229ca8ac2db3c050d5cd3debb5b353d
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-01-30 22:45:12 +0000

    !!! flake.{nix,lock}: update pyk

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       "github:ethereum/legacytests/d7abc42a7b352a7b44b1f66b58aca54e4af6a9d7";
     ethereum-legacytests.flake = false;
     haskell-backend.follows = "k-framework/haskell-backend";
-    pyk.url = "github:runtimeverification/pyk/v0.1.105";
+    pyk.url = "github:runtimeverification/pyk/single-step";
     pyk.inputs.flake-utils.follows = "k-framework/flake-utils";
     pyk.inputs.nixpkgs.follows = "k-framework/nixpkgs";
 
