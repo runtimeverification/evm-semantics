c2ce8e93ec456b3ecb4839a4fa3974f97b7772ee
Tamas Toth <tamas.toth@runtimeverification.com>
2023-02-24 14:01:10 +0100

    Extract function `foundry_prove`

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -13,15 +13,14 @@ from pyk.kast.outer import KDefinition, KFlatModule, KImport, KRequire, KRuleLik
 from pyk.kcfg import KCFG, KCFGExplore, KCFGViewer
 from pyk.ktool.kompile import KompileBackend
 from pyk.ktool.krun import KRunOutput, _krun
-from pyk.prelude.k import GENERATED_TOP_CELL
 from pyk.prelude.ml import mlTop
 from pyk.utils import shorten_hashes, single
 
-from .foundry import foundry_kompile
+from .foundry import foundry_kompile, foundry_prove
 from .gst_to_kore import gst_to_kore
 from .kevm import KEVM, Foundry
-from .solc_to_k import Contract, contract_to_main_module, method_to_cfg, solc_compile
-from .utils import KDefinition__expand_macros, arg_pair_of, find_free_port, parallel_kcfg_explore
+from .solc_to_k import Contract, contract_to_main_module, solc_compile
+from .utils import arg_pair_of, find_free_port, parallel_kcfg_explore
 
 T = TypeVar('T')
 
@@ -175,24 +174,6 @@ def exec_foundry_kompile(
     )
 
 
-def _contract_json_paths(foundry_out: Path) -> List[str]:
-    pattern = '*.sol/*.json'
-    paths = foundry_out.glob(pattern)
-    json_paths = [str(path) for path in paths]
-    json_paths = [json_path for json_path in json_paths if not json_path.endswith('.metadata.json')]
-    json_paths = sorted(json_paths)  # Must sort to get consistent output order on different platforms
-    return json_paths
-
-
-def _contract_from_json(json_path: str) -> Contract:
-    _LOGGER.info(f'Processing contract file: {json_path}')
-    with open(json_path, 'r') as json_file:
-        contract_json = json.loads(json_file.read())
-    contract_name = json_path.split('/')[-1]
-    contract_name = contract_name[0:-5] if contract_name.endswith('.json') else contract_name
-    return Contract(contract_name, contract_json, foundry=True)
-
-
 def exec_prove(
     definition_dir: Path,
     profile: bool,
@@ -310,101 +291,22 @@ def exec_foundry_prove(
     _ignore_arg(kwargs, 'syntax_module', f'--syntax-module: {kwargs["syntax_module"]}')
     _ignore_arg(kwargs, 'definition_dir', f'--definition: {kwargs["definition_dir"]}')
     _ignore_arg(kwargs, 'spec_module', f'--spec-module: {kwargs["spec_module"]}')
-    if workers <= 0:
-        raise ValueError(f'Must have at least one worker, found: --workers {workers}')
-    if max_iterations is not None and max_iterations < 0:
-        raise ValueError(f'Must have a non-negative number of iterations, found: --max-iterations {max_iterations}')
-    definition_dir = foundry_out / 'kompiled'
-    use_directory = foundry_out / 'specs'
-    use_directory.mkdir(parents=True, exist_ok=True)
-    kcfgs_dir = foundry_out / 'kcfgs'
-    if not kcfgs_dir.exists():
-        kcfgs_dir.mkdir()
-    br = BugReport(foundry_out / 'bug_report') if bug_report else None
-    foundry = Foundry(definition_dir, profile=profile, use_directory=use_directory, bug_report=br)
-
-    json_paths = _contract_json_paths(foundry_out)
-    contracts = [_contract_from_json(json_path) for json_path in json_paths]
-    all_tests = [
-        f'{contract.name}.{method.name}'
-        for contract in contracts
-        if contract.name.endswith('Test')
-        for method in contract.methods
-        if method.name.startswith('test')
-    ]
-    all_non_tests = [
-        f'{contract.name}.{method.name}'
-        for contract in contracts
-        for method in contract.methods
-        if f'{contract.name}.{method.name}' not in all_tests
-    ]
-    unfound_tests: List[str] = []
-    tests = list(tests)
-    if not tests:
-        tests = all_tests
-    for _t in tests:
-        if _t not in (all_tests + all_non_tests):
-            unfound_tests.append(_t)
-    for _t in exclude_tests:
-        if _t not in all_tests:
-            unfound_tests.append(_t)
-        if _t in tests:
-            tests.remove(_t)
-    _LOGGER.info(f'Running tests: {tests}')
-    if unfound_tests:
-        raise ValueError(f'Test identifiers not found: {unfound_tests}')
-
-    kcfgs: Dict[str, KCFG] = {}
-    for test in tests:
-        kcfg = KCFGExplore.read_cfg(test, kcfgs_dir)
-        if kcfg is not None and not reinit:
-            kcfgs[test] = kcfg
-        else:
-            _LOGGER.info(f'Initializing KCFG for test: {test}')
-            contract_name, method_name = test.split('.')
-            contract = [c for c in contracts if c.name == contract_name][0]
-            method = [m for m in contract.methods if m.name == method_name][0]
-            empty_config = foundry.definition.empty_config(GENERATED_TOP_CELL)
-            kcfg = method_to_cfg(empty_config, contract, method)
-            init_term = kcfg.get_unique_init().cterm.kast
-            target_term = kcfg.get_unique_target().cterm.kast
-            _LOGGER.info(f'Expanding macros in initial state for test: {test}')
-            init_term = KDefinition__expand_macros(foundry.definition, init_term)
-            init_cterm = KEVM.add_invariant(CTerm(init_term))
-            _LOGGER.info(f'Expanding macros in target state for test: {test}')
-            target_term = KDefinition__expand_macros(foundry.definition, target_term)
-            target_cterm = KEVM.add_invariant(CTerm(target_term))
-            kcfg.replace_node(kcfg.get_unique_init().id, init_cterm)
-            kcfg.replace_node(kcfg.get_unique_target().id, target_cterm)
-            if simplify_init:
-                with KCFGExplore(foundry, port=find_free_port(), bug_report=br) as kcfg_explore:
-                    kcfg = kcfg_explore.simplify(test, kcfg)
-            kcfgs[test] = kcfg
-            KCFGExplore.write_cfg(test, kcfgs_dir, kcfg)
-
-    results = parallel_kcfg_explore(
-        foundry,
-        kcfgs,
-        save_directory=kcfgs_dir,
+    foundry_prove(
+        profile=profile,
+        foundry_out=foundry_out,
         max_depth=max_depth,
         max_iterations=max_iterations,
+        reinit=reinit,
+        tests=tests,
+        exclude_tests=exclude_tests,
         workers=workers,
+        simplify_init=simplify_init,
         break_every_step=break_every_step,
         break_on_calls=break_on_calls,
         implication_every_block=implication_every_block,
         rpc_base_port=rpc_base_port,
-        is_terminal=KEVM.is_terminal,
-        extract_branches=KEVM.extract_branches,
-        bug_report=br,
+        bug_report=bug_report,
     )
-    failed = 0
-    for pid, r in results.items():
-        if r:
-            print(f'PROOF PASSED: {pid}')
-        else:
-            failed += 1
-            print(f'PROOF FAILED: {pid}')
-    sys.exit(failed)
 
 
 def exec_foundry_show(
--- kevm-pyk/src/kevm_pyk/foundry.py
+++ kevm-pyk/src/kevm_pyk/foundry.py
@@ -1,14 +1,20 @@
 import json
 import logging
+import sys
 from pathlib import Path
-from typing import Final, Iterable, List, Optional
+from typing import Dict, Final, Iterable, List, Optional
 
+from pyk.cli_utils import BugReport
+from pyk.cterm import CTerm
 from pyk.kast.inner import KInner
 from pyk.kast.outer import KDefinition, KFlatModule, KImport, KRequire
+from pyk.kcfg import KCFG, KCFGExplore
 from pyk.ktool.kompile import KompileBackend
+from pyk.prelude.k import GENERATED_TOP_CELL
 
 from .kevm import KEVM, Foundry
-from .solc_to_k import Contract, contract_to_main_module
+from .solc_to_k import Contract, contract_to_main_module, method_to_cfg
+from .utils import KDefinition__expand_macros, find_free_port, parallel_kcfg_explore
 
 _LOGGER: Final = logging.getLogger(__name__)
 
@@ -85,6 +91,124 @@ def foundry_kompile(
             ccopts=ccopts,
             llvm_kompile=llvm_kompile,
         )
+
+
+def foundry_prove(
+    profile: bool,
+    foundry_out: Path,
+    max_depth: int = 100,
+    max_iterations: Optional[int] = None,
+    reinit: bool = False,
+    tests: Iterable[str] = (),
+    exclude_tests: Iterable[str] = (),
+    workers: int = 1,
+    simplify_init: bool = True,
+    break_every_step: bool = False,
+    break_on_calls: bool = True,
+    implication_every_block: bool = True,
+    rpc_base_port: Optional[int] = None,
+    bug_report: bool = False,
+) -> None:
+    if workers <= 0:
+        raise ValueError(f'Must have at least one worker, found: --workers {workers}')
+    if max_iterations is not None and max_iterations < 0:
+        raise ValueError(f'Must have a non-negative number of iterations, found: --max-iterations {max_iterations}')
+    definition_dir = foundry_out / 'kompiled'
+    use_directory = foundry_out / 'specs'
+    use_directory.mkdir(parents=True, exist_ok=True)
+    kcfgs_dir = foundry_out / 'kcfgs'
+    if not kcfgs_dir.exists():
+        kcfgs_dir.mkdir()
+    br = BugReport(foundry_out / 'bug_report') if bug_report else None
+    foundry = Foundry(definition_dir, profile=profile, use_directory=use_directory, bug_report=br)
+
+    json_paths = _contract_json_paths(foundry_out)
+    contracts = [_contract_from_json(json_path) for json_path in json_paths]
+    all_tests = [
+        f'{contract.name}.{method.name}'
+        for contract in contracts
+        if contract.name.endswith('Test')
+        for method in contract.methods
+        if method.name.startswith('test')
+    ]
+    all_non_tests = [
+        f'{contract.name}.{method.name}'
+        for contract in contracts
+        for method in contract.methods
+        if f'{contract.name}.{method.name}' not in all_tests
+    ]
+    unfound_tests: List[str] = []
+    tests = list(tests)
+    if not tests:
+        tests = all_tests
+    for _t in tests:
+        if _t not in (all_tests + all_non_tests):
+            unfound_tests.append(_t)
+    for _t in exclude_tests:
+        if _t not in all_tests:
+            unfound_tests.append(_t)
+        if _t in tests:
+            tests.remove(_t)
+    _LOGGER.info(f'Running tests: {tests}')
+    if unfound_tests:
+        raise ValueError(f'Test identifiers not found: {unfound_tests}')
+
+    kcfgs: Dict[str, KCFG] = {}
+    for test in tests:
+        kcfg = KCFGExplore.read_cfg(test, kcfgs_dir)
+        if kcfg is not None and not reinit:
+            kcfgs[test] = kcfg
+        else:
+            _LOGGER.info(f'Initializing KCFG for test: {test}')
+            contract_name, method_name = test.split('.')
+            contract = [c for c in contracts if c.name == contract_name][0]
+            method = [m for m in contract.methods if m.name == method_name][0]
+            empty_config = foundry.definition.empty_config(GENERATED_TOP_CELL)
+            kcfg = method_to_cfg(empty_config, contract, method)
+            init_term = kcfg.get_unique_init().cterm.kast
+            target_term = kcfg.get_unique_target().cterm.kast
+            _LOGGER.info(f'Expanding macros in initial state for test: {test}')
+            init_term = KDefinition__expand_macros(foundry.definition, init_term)
+            init_cterm = KEVM.add_invariant(CTerm(init_term))
+            _LOGGER.info(f'Expanding macros in target state for test: {test}')
+            target_term = KDefinition__expand_macros(foundry.definition, target_term)
+            target_cterm = KEVM.add_invariant(CTerm(target_term))
+            kcfg.replace_node(kcfg.get_unique_init().id, init_cterm)
+            kcfg.replace_node(kcfg.get_unique_target().id, target_cterm)
+            if simplify_init:
+                with KCFGExplore(foundry, port=find_free_port(), bug_report=br) as kcfg_explore:
+                    kcfg = kcfg_explore.simplify(test, kcfg)
+            kcfgs[test] = kcfg
+            KCFGExplore.write_cfg(test, kcfgs_dir, kcfg)
+
+    results = parallel_kcfg_explore(
+        foundry,
+        kcfgs,
+        save_directory=kcfgs_dir,
+        max_depth=max_depth,
+        max_iterations=max_iterations,
+        workers=workers,
+        break_every_step=break_every_step,
+        break_on_calls=break_on_calls,
+        implication_every_block=implication_every_block,
+        rpc_base_port=rpc_base_port,
+        is_terminal=KEVM.is_terminal,
+        extract_branches=KEVM.extract_branches,
+        bug_report=br,
+    )
+    failed = 0
+    for pid, r in results.items():
+        if r:
+            print(f'PROOF PASSED: {pid}')
+        else:
+            failed += 1
+            print(f'PROOF FAILED: {pid}')
+    sys.exit(failed)
+
+
+def _write_cfg(cfg: KCFG, path: Path) -> None:
+    path.write_text(cfg.to_json())
+    _LOGGER.info(f'Updated CFG file: {path}')
 
 
 def _contract_json_paths(foundry_out: Path) -> List[str]:
