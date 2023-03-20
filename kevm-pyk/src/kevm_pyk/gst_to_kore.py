8ff2d622389106ce2b6e7d669341103140b2206c
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-20 11:37:32 +0100

    Extract `SortApp`s and `SymbolId`s in `gst_to_kore`

--- kevm-pyk/src/kevm_pyk/gst_to_kore.py
+++ kevm-pyk/src/kevm_pyk/gst_to_kore.py
@@ -2,76 +2,86 @@ import logging
 from functools import reduce
 from typing import Any, Final, Optional
 
-from pyk.kore.prelude import INT, STRING, int_dv, string_dv
-from pyk.kore.syntax import DV, App, Pattern, Sort, SortApp, String
+from pyk.kore.prelude import INT, LBL_MAP, LBL_MAP_ITEM, STOP_MAP, STRING, int_dv, string_dv
+from pyk.kore.syntax import DV, App, Pattern, Sort, SortApp, String, SymbolId
 
 _LOGGER: Final = logging.getLogger(__name__)
+
+SORT_K_CONFIG_VAR: Final = SortApp('SortKConfigVar')
+SORT_K_ITEM: Final = SortApp('SortKItem')
+SORT_JSON: Final = SortApp('SortJSON')
+SORT_JSON_KEY: Final = SortApp('SortJSONKey')
+
+LBL_JSONS: Final = SymbolId('LblJSONs')
+LBL_JSON_LIST: Final = SymbolId('LblJSONList')
+LBL_JSON_OBJECT: Final = SymbolId('LblJSONObject')
+LBL_JSON_ENTRY: Final = SymbolId('LblJSONEntry')
+INJ: Final = SymbolId('inj')
+
+STOP_JSONS: Final = App("Lbl'Stop'List'LBraQuot'JSONs'QuotRBraUnds'JSONs")
 
 
 def gst_to_kore(gst_data: Any, schedule: str, mode: str, chainid: int) -> App:
     entries = (
-        _config_map_entry('PGM', _json_to_kore(gst_data), SortApp('SortJSON')),
+        _config_map_entry('PGM', _json_to_kore(gst_data), SORT_JSON),
         _config_map_entry('SCHEDULE', _schedule_to_kore(schedule), SortApp('SortSchedule')),
         _config_map_entry('MODE', _mode_to_kore(mode), SortApp('SortMode')),
-        _config_map_entry('CHAINID', _chainid_to_kore(chainid), SortApp('SortInt')),
+        _config_map_entry('CHAINID', _chainid_to_kore(chainid), INT),
     )
     return App(
         'LblinitGeneratedTopCell',
         (),
-        (reduce(lambda x, y: App("Lbl'Unds'Map'Unds'", (), (x, y)), entries, App("Lbl'Stop'Map")),),
+        (reduce(lambda x, y: App(LBL_MAP, (), (x, y)), entries, STOP_MAP),),
     )
 
 
 def _config_map_entry(var: str, value: Pattern, sort: Sort) -> App:
     return App(
-        "Lbl'UndsPipe'-'-GT-Unds'",
+        LBL_MAP_ITEM,
         (),
         (
-            _sort_injection(SortApp('SortKConfigVar'), SortApp('SortKItem'), _k_config_var(var)),
-            _sort_injection(sort, SortApp('SortKItem'), value),
+            _sort_injection(SORT_K_CONFIG_VAR, SORT_K_ITEM, _k_config_var(var)),
+            _sort_injection(sort, SORT_K_ITEM, value),
         ),
     )
 
 
 def _sort_injection(sort1: Sort, sort2: Sort, pattern: Pattern) -> App:
-    return App('inj', (sort1, sort2), (pattern,))
+    return App(INJ, (sort1, sort2), (pattern,))
 
 
 def _k_config_var(_data: str) -> DV:
-    return DV(SortApp('SortKConfigVar'), String(f'${_data}'))
+    return DV(SORT_K_CONFIG_VAR, String(f'${_data}'))
 
 
 def _json_to_kore(_data: Any, *, sort: Optional[Sort] = None) -> Pattern:
     if sort is None:
-        sort = SortApp('SortJSON')
+        sort = SORT_JSON
 
     if isinstance(_data, list):
         return App(
-            'LblJSONList',
+            LBL_JSON_LIST,
             (),
             (
                 reduce(
-                    lambda x, y: App('LblJSONs', (), (y, x)),
+                    lambda x, y: App(LBL_JSONS, (), (y, x)),
                     reversed([_json_to_kore(elem) for elem in _data]),
-                    App("Lbl'Stop'List'LBraQuot'JSONs'QuotRBraUnds'JSONs"),
+                    STOP_JSONS,
                 ),
             ),
         )
 
     if isinstance(_data, dict):
         return App(
-            'LblJSONObject',
+            LBL_JSON_OBJECT,
             (),
             (
                 reduce(
-                    lambda x, y: App('LblJSONs', (), (App('LblJSONEntry', (), (y[0], y[1])), x)),
+                    lambda x, y: App(LBL_JSONS, (), (App(LBL_JSON_ENTRY, (), (y[0], y[1])), x)),
                     reversed(
-                        [
-                            (_json_to_kore(key, sort=SortApp('SortJSONKey')), _json_to_kore(value))
-                            for key, value in _data.items()
-                        ]
+                        [(_json_to_kore(key, sort=SORT_JSON_KEY), _json_to_kore(value)) for key, value in _data.items()]
                     ),
-                    App("Lbl'Stop'List'LBraQuot'JSONs'QuotRBraUnds'JSONs"),
+                    STOP_JSONS,
                 ),
             ),
         )
