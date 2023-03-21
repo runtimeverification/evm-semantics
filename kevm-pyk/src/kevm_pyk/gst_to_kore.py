7fcbb9cb53e7901c35bb14a2660ebe2cc37d6800
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-21 11:06:45 +0100

    Extract command `gst-to-kore` from `kevm-pyk`

--- kevm-pyk/src/kevm_pyk/gst_to_kore.py
+++ kevm-pyk/src/kevm_pyk/gst_to_kore.py
@@ -1,11 +1,17 @@
+import json
 import logging
+import sys
+from argparse import ArgumentParser, Namespace
 from functools import reduce
+from pathlib import Path
 from typing import Any, Final, Optional
 
+from pyk.cli_utils import file_path
 from pyk.kore.prelude import INT, LBL_MAP, LBL_MAP_ITEM, STOP_MAP, STRING, int_dv, string_dv
 from pyk.kore.syntax import DV, App, Pattern, Sort, SortApp, String, SymbolId
 
 _LOGGER: Final = logging.getLogger(__name__)
+_LOG_FORMAT: Final = '%(levelname)s %(asctime)s %(name)s - %(message)s'
 
 SORT_K_CONFIG_VAR: Final = SortApp('SortKConfigVar')
 SORT_K_ITEM: Final = SortApp('SortKItem')
@@ -105,3 +111,55 @@ def _chainid_to_kore(chainid: int) -> DV:
 
 def _mode_to_kore(mode: str) -> App:
     return App(f'Lbl{mode}')
+
+
+def main() -> None:
+    sys.setrecursionlimit(15000000)
+    args = _parse_args()
+    _exec_gst_to_kore(args.input_file, args.schedule, args.mode, args.chainid)
+
+
+def _exec_gst_to_kore(input_file: Path, schedule: str, mode: str, chainid: int) -> None:
+    gst_data = json.loads(input_file.read_text())
+    kore = gst_to_kore(gst_data, schedule, mode, chainid)
+    kore.write(sys.stdout)
+    sys.stdout.write('\n')
+    _LOGGER.info('Finished writing KORE')
+
+
+def _parse_args() -> Namespace:
+    schedules = (
+        'DEFAULT',
+        'FRONTIER',
+        'HOMESTEAD',
+        'TANGERINE_WHISTLE',
+        'SPURIOUS_DRAGON',
+        'BYZANTIUM',
+        'CONSTANTINOPLE',
+        'PETERSBURG',
+        'ISTANBUL',
+        'BERLIN',
+        'LONDON',
+    )
+    modes = ('NORMAL', 'VMTESTS')
+
+    parser = ArgumentParser(description='Convert a GeneralStateTest to Kore for compsumption by KEVM')
+    parser.add_argument('input_file', type=file_path, help='path to GST')
+    parser.add_argument(
+        '--schedule',
+        choices=schedules,
+        default='LONDON',
+        help=f"schedule to use for execution [{'|'.join(schedules)}]",
+    )
+    parser.add_argument('--chainid', type=int, default=1, help='chain ID to use for execution')
+    parser.add_argument(
+        '--mode',
+        choices=modes,
+        default='NORMAL',
+        help="execution mode to use [{'|'.join(modes)}]",
+    )
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
