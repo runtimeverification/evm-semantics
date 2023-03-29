7d4449fb0e3032c5359ce05d3a8fd550cc3fb2fe
Lucas MT <lucas.tabajara@runtimeverification.com>
2023-03-29 11:19:25 -0500

    Reformatting

--- kevm-pyk/src/kevm_pyk/foundry.py
+++ kevm-pyk/src/kevm_pyk/foundry.py
@@ -164,7 +164,7 @@ class Foundry:
 
     @staticmethod
     def address_TEST_CONTRACT() -> KToken:  # noqa: N802
-        return intToken(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496)
+        return intToken(0x7FA9385BE102AC3EAC297483DD6233D62B3E1496)
 
     @staticmethod
     def account_TEST_CONTRACT_ADDRESS() -> KApply:  # noqa: N802
