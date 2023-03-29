64f5e93d503cb2e4759275229bc136e6f067d38b
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-03-29 02:27:35 +0000

    tests/foundry/test-foundry: updates for CLI

--- tests/foundry/test-foundry.sh
+++ tests/foundry/test-foundry.sh
@@ -2,11 +2,14 @@
 
 set -xueo pipefail
 
-forge build                                                                                         >&2
-kevm foundry-kompile --foundry-project-root tests/foundry                                           >&2
-kevm foundry-prove --foundry-project-root tests/foundry --test UintTypeTest.test_uint256 --depth 40 >&2 || true
+foundry_args=(--foundry-project-root tests/foundry --verbose)
+test=UintTypeTest.test_uint256
 
-kevm foundry-list --foundry-project-root tests/foundry
-kevm foundry-show --foundry-project-root tests/foundry UintTypeTest.test_uint256 --node '151838..8b4053' \
-    --to-module --omit-unstable-output --frontier --stuck                                            \
+( cd tests/foundry && forge build ; )                             >&2
+kevm foundry-kompile "${foundry_args[@]}"                         >&2
+kevm foundry-prove "${foundry_args[@]}" --test ${test} --depth 40 >&2 || true
+
+kevm foundry-list "${foundry_args[@]}"
+kevm foundry-show "${foundry_args[@]}" ${test} --node '151838..8b4053' \
+    --to-module --omit-unstable-output --frontier --stuck              \
     grep --invert-match -e 'rule \[BASIC-BLOCK-' -e '\[priority(.*), label(BASIC-BLOCK-'
