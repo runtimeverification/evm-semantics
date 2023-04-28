fa0bc5cc3dc73d0300192a930e1c7999de89e4a3
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-04-28 13:48:11 +0200

    Locate `KEVM_LIB` if not set

--- kevm-pyk/src/kevm_pyk/config.py
+++ kevm-pyk/src/kevm_pyk/config.py
@@ -2,25 +2,40 @@ from __future__ import annotations
 
 import os
 from pathlib import Path
+from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
 
-from pyk.cli_utils import check_dir_path
+from pyk.cli_utils import check_dir_path, run_process
 
 if TYPE_CHECKING:
     from typing import Final
 
 
-def _env_or_raise(var: str) -> str:
-    env = os.getenv(var)
-    if env is None:
-        raise ValueError(f'Environment variable not set: {var}')
-    return env
+def _kevm_lib() -> Path:
+    kevm_lib: Path
+
+    env = os.getenv('KEVM_LIB')
+    if env is not None:
+        kevm_lib = Path(env).resolve()
+        check_dir_path(kevm_lib)
+        return kevm_lib
+    else:
+        try:
+            which_kevm = run_process(('which', 'kevm'), pipe_stderr=True).stdout.strip()
+        except CalledProcessError:
+            raise RuntimeError(
+                "Couldn't locate KEVM libraries. Either set the KEVM_LIB environment variable, or put kevm on PATH."
+            ) from None
+
+        kevm_path = Path(which_kevm).resolve()
+        kevm_lib = kevm_path.parents[1] / 'lib/kevm'
+
+    check_dir_path(kevm_lib)
+    return kevm_lib
 
 
 NIX_BUILD: Final = bool(os.getenv('NIX_BUILD'))
-
-KEVM_LIB: Final = Path(_env_or_raise('KEVM_LIB')).resolve()
-check_dir_path(KEVM_LIB)
+KEVM_LIB: Final = _kevm_lib()
 
 INCLUDE_DIR: Final = KEVM_LIB / 'include/kframework'
 check_dir_path(INCLUDE_DIR)
