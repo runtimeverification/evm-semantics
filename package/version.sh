f5c1795aea0c7d6781c94f0e6d4c434ad3ad1982
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-05-11 11:23:58 -0600

    Publish dockerhub images on CI (#1747)
    * .github/master-push, package/docker/Dockerfile: first attempt at making Docker image with KEVM pre-installed
    
    * .github/master-push, package/debian: enable building Jammy package on CI
    
    * .github/master-push: correct name
    
    * .github/master-push: correct image name to run on
    
    * Makefile: correct version used
    
    * package/version.sh: also update debian package version id
    
    * kevm-pyk/: thread through --read-only-kompiled-directory to kevm kompile
    
    * package/debian/control.jammy: adjust
    
    * .github/Dockerfile: add Build-Depends to docker image
    
    * .github/{with-docker,Dockerfile}: run all CI in subdirectory of home called workspace
    
    * Makefile: remove git command from distclean target
    
    * package/debian/rules: assume that kframework is already in place
    
    * package/debian/rules: build plugin and libff ahead of time
    
    * package/debian/rules: also build kevm-pyk ahead of time
    
    * .github/master-push: run on private runners for better docker caching
    
    * package/debian/rules: skip node build
    
    * package/debian/compat: up the compatibility level to 10!
    
    * package/debian/rules: skip llvm build too
    
    * package/debian/rules: no need for libff for symbolic execution
    
    * .github/master-push: set up debian package testing
    
    * package/debian/Dockerfile.test: use jammy
    
    * package/debian/Dockerfile.test: include foundry
    
    * .github/master-push, package/debian/Dockerfile.test: make sure K is installed
    
    * .github/master-push: syntax fix
    
    * .github/master-push: add test of Foundry for debian image
    
    * .github/master-push: correct quoting
    
    * .github/master-push: correct test name
    
    * .github/master-push: simpler way to get foundry property test suite to test
    
    * .github/master-push: typo
    
    * .github/master-push: correct permissions
    
    * .github/master-push: recursive clone KEVM submodule
    
    * .github/master-push, package/docker/Dockerfile: build in poetry/kevm-pyk directly in Docker image instead
    
    * .github/master-push, package/docker/Dockerfile: more adjustments for install
    
    * package/docker/Dockerfile: COPY => ADD
    
    * package/debian/rules: make sure to build-kevm-pyk too
    
    * package/docker/Dockerfile: remove kevm-pyk installation step
    
    * package/docker/Dockerfile: working to install KEVM
    
    * .github/master-push: correct test name
    
    * .github/master-push: also list and show kcfg for testing
    
    * .github/master-push: publish dockerhub images too
    
    * .github/master-push: correct command
    
    * .github/master-push: correct docker image tag name
    
    * .github/master-push: further adjustments to tag_name
    
    * .github/master-push: remove unused variable
    
    * !!! .github/master-push: debugging
    
    * !!! .github/test-pr: debugging
    
    * package/debian/rules: use pip to install kevm-pyk
    
    * Set Version: 1.0.151
    
    * kevm-pyk/: sync poetry files
    
    * package/debian/control.jammy: require python3-pip at build time
    
    * package/debian/rules: install using pip3, not pip
    
    * package/debian/rules: remove build-kevm-pyk target
    
    * package/debian/rules: do not attempt pip install in dh_auto_install
    
    * package/docker: install kevm-pyk with pip in docker image
    
    * Set Version: 1.0.151
    
    * package/docker/Dockerfile: make sure pip3 is installed
    
    * Revert "!!! .github/test-pr: debugging"
    
    This reverts commit c5c29b70b0f1d5a2f13a04d72d6d7d6c12e64104.
    
    * Revert "!!! .github/master-push: debugging"
    
    This reverts commit 23f1b5be2bc89bbdd2bca77c24c7ef51e8b6fa7b.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- package/version.sh
+++ package/version.sh
@@ -29,6 +29,7 @@ version_sub() {
     local version
     version="$(cat $version_file)"
     sed --in-place 's/^version = ".*"$/version = "'${version}'"/' kevm-pyk/pyproject.toml
+    sed --in-place 's/^kevm (.*) unstable; urgency=medium$/kevm ('${version}') unstable; urgency=medium/' package/debian/changelog
 }
 
 version_command="$1" ; shift
