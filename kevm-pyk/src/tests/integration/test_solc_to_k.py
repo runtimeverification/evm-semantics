3c063a3e6b096b852ceaba36387ddbfaa18a7240
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-06-13 13:43:54 +0200

    Extract fixture `bin_runtime`

--- kevm-pyk/src/tests/integration/test_solc_to_k.py
+++ kevm-pyk/src/tests/integration/test_solc_to_k.py
@@ -5,14 +5,10 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from kevm_pyk import config
-from kevm_pyk.__main__ import exec_solc_to_k
 from kevm_pyk.kompile import KompileTarget, kevm_kompile
 
 if TYPE_CHECKING:
-    from typing import Final
-
-    from pytest import CaptureFixture
+    from typing import Callable, Final
 
 
 EXAMPLES_DIR: Final = (Path(__file__).parent / 'test-data/examples').resolve(strict=True)
@@ -24,30 +20,16 @@ TEST_DATA: Final = tuple(EXAMPLES_DIR.glob('*.sol'))
     TEST_DATA,
     ids=[contract_file.name for contract_file in TEST_DATA],
 )
-def test_solc_to_k(contract_file: Path, tmp_path: Path, capsys: CaptureFixture[str]) -> None:
+def test_solc_to_k(contract_file: Path, bin_runtime: Callable, tmp_path: Path) -> None:
     # Given
-    contract_name = contract_file.stem
-    main_module_name = f'{contract_name.upper()}-VERIFICATION'
-    main_file = tmp_path / f'{contract_name.lower()}-bin-runtime.k'
     definition_dir = tmp_path / 'kompiled'
+    main_file, main_module_name = bin_runtime(contract_file)
 
     # When
-    exec_solc_to_k(  # TODO extract library function from command line entrypoint
-        definition_dir=config.HASKELL_DIR,
-        contract_file=contract_file,
-        contract_name=contract_name,
-        main_module=main_module_name,
-        requires=[],
-        imports=[],
-    )
-
-    k_text = capsys.readouterr().out
-    main_file.write_text(k_text)
-
     kevm_kompile(
         target=KompileTarget.HASKELL,
-        main_file=main_file,
         output_dir=definition_dir,
+        main_file=main_file,
         main_module=main_module_name,
         syntax_module=main_module_name,
     )
