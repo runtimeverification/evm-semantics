3c45794a85c60cccc685146202d2e3ef1bfea434
franfran <fgg67@hotmail.fr>
2023-06-16 13:09:39 +0300

    newline

--- tests/foundry/test/DynamicTypes.t.sol
+++ tests/foundry/test/DynamicTypes.t.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.13;
 import "forge-std/Test.sol";
 
 contract DynamicTypesTest is Test {
+
     function test_dynamic_byte_read(bytes memory data, uint256 offset) public {
         uint8 mydata = uint8(data[offset]);
         vm.assume(mydata < 3);
