5ad8966c626f2ba3aefdd1be855b593e53e23e11
François Guyot <francois.guyot@runtimeverification.com>
2023-06-21 07:22:14 +0000

    use uint256

--- tests/foundry/test/FreshInt.t.sol
+++ tests/foundry/test/FreshInt.t.sol
@@ -7,23 +7,23 @@ import "../src/KEVMCheats.sol";
 contract FreshIntTest is Test, KEVMCheats {
     function test_uint256() public {
         kevm.infiniteGas();
-        uint256 fresh_uint256 = kevm.freshUInt(32);
+        uint256 fresh_uint256 = uint256(kevm.freshUInt(32));
         assertGe(fresh_uint256, type(uint256).min);
         assertLe(fresh_uint256, type(uint256).max);
     }
 
-    function test_uint128() public {
-        kevm.infiniteGas();
-        uint256 fresh_uint128 = kevm.freshUInt(16);
-        assertGe(fresh_uint128, type(uint128).min);
-        assertLe(fresh_uint128, type(uint128).max);
-    }
+    // function test_uint128() public {
+    //     kevm.infiniteGas();
+    //     uint256 fresh_uint128 = kevm.freshUInt(16);
+    //     assertGe(fresh_uint128, type(uint128).min);
+    //     assertLe(fresh_uint128, type(uint128).max);
+    // }
 
     function test_int128() public {
         kevm.infiniteGas();
         uint256 fresh_uint128 = kevm.freshUInt(16);
         assertLe(fresh_uint128, type(uint128).max);
-        int128 fresh_int128 = int128(uint128(fresh_uint128));
+        int128 fresh_int128 = int128(uint128(uint256(fresh_uint128)));
         assertGe(fresh_int128, type(int128).min);
         assertLe(fresh_int128, type(int128).max);
     }
@@ -36,11 +36,13 @@ contract FreshIntTest is Test, KEVMCheats {
     }
 
     function test_int() public {
-        int128 s;
-        uint256 u = kevm.freshUInt(16);
-        assembly {
-            s := u
-        }
+        kevm.infiniteGas();
+        // int128 s;
+        // uint256 u = uint256(kevm.freshUInt(16));
+        int128 s = int128(uint128(uint256(kevm.freshUInt(16))));
+        // assembly {
+        //     s := u
+        // }
         assertGe(s, type(int128).min);
         assertLe(s, type(int128).max);
     }
