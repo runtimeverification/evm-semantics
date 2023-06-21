d8c54c8e4237d1fa6bb6a816e2de42547dacb50a
François Guyot <francois.guyot@runtimeverification.com>
2023-06-21 12:06:30 +0000

    Revert "update nonsense tests"
    This reverts commit fcc5705564df16cfcad3ff93de5b333259c10a88.

--- tests/foundry/test/AssumeTest.t.sol
+++ tests/foundry/test/AssumeTest.t.sol
@@ -12,7 +12,7 @@ contract AssumeTest is Test {
 
     function test_assume_false(uint256 a, uint256 b) public {
         vm.assume(a != b);
-        assertFalse(a == b);
+        assertEq(a, b);
     }
 
     function testFail_assume_true(uint256 a, uint256 b) public {
@@ -22,7 +22,7 @@ contract AssumeTest is Test {
 
     function testFail_assume_false(uint256 a, uint256 b) public {
         vm.assume(a == b);
-        assertFalse(a == b);
+        assertEq(a, b);
     }
 
     function test_assume_staticCall(bool a) public {
