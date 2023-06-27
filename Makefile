runtimeverification/evm-semantics

commit 2dc77d846257fe1cdfb28b497c99418b46379e51

Port prover tests to Python (#1894)
* Fix `claim_labels` type hint and default value

* Port prover tests to Python

* Remove proof-related targets from `Makefile`

* Fix `main_file` selection logic for kompilation

* Cache kompiled definitions

* Increase timeout for concrete execution tests

* Set Version: 1.0.206

---------

Co-authored-by: devops <devops@runtimeverification.com>

Makefile
@@ -44,19 +44,18 @@ PLUGIN_FULL_PATH := $(abspath ${PLUGIN_SUBMODULE})
 export PLUGIN_FULL_PATH
 
 
-.PHONY: all clean distclean                                                                                                                  \
-        deps k-deps plugin-deps protobuf                                                                                                     \
-        build build-haskell build-haskell-standalone build-foundry build-llvm build-prove build-prove-haskell build-node build-kevm          \
-        test test-all test-conformance test-rest-conformance test-all-conformance test-slow-conformance test-failing-conformance             \
-        test-vm test-rest-vm test-all-vm test-bchain test-rest-bchain test-all-bchain test-node                                              \
-        test-prove test-failing-prove test-foundry-kcfg-diff                                                                                 \
-        test-prove-benchmarks test-prove-functional test-prove-opcodes test-prove-erc20 test-prove-bihu test-prove-examples test-prove-smoke \
-        test-prove-mcd test-klab-prove                                                                                                       \
-        test-parse test-failure test-foundry-kompile test-foundry-prove test-foundry-bmc-prove test-foundry-list                             \
-        test-interactive test-interactive-help test-interactive-run test-interactive-prove test-interactive-search                           \
-        test-kevm-pyk foundry-forge-build foundry-forge-test foundry-clean foundry-fail                                                      \
-        media media-pdf metropolis-theme                                                                                                     \
-        install uninstall                                                                                                                    \
+.PHONY: all clean distclean                                                                                                      \
+        deps k-deps plugin-deps protobuf                                                                                         \
+        build build-haskell build-haskell-standalone build-foundry build-llvm build-node build-kevm                              \
+        test test-all test-conformance test-rest-conformance test-all-conformance test-slow-conformance test-failing-conformance \
+        test-vm test-rest-vm test-all-vm test-bchain test-rest-bchain test-all-bchain test-node                                  \
+        test-prove test-failing-prove test-foundry-kcfg-diff                                                                     \
+        test-prove-smoke test-klab-prove                                                                                         \
+        test-parse test-failure test-foundry-kompile test-foundry-prove test-foundry-bmc-prove test-foundry-list                 \
+        test-interactive test-interactive-help test-interactive-run test-interactive-prove test-interactive-search               \
+        test-kevm-pyk foundry-forge-build foundry-forge-test foundry-clean foundry-fail                                          \
+        media media-pdf metropolis-theme                                                                                         \
+        install uninstall                                                                                                        \
         poetry-env poetry shell kevm-pyk
 .SECONDARY:
 
@@ -410,35 +409,6 @@ test: test-conformance test-prove test-interactive test-parse test-kevm-pyk
 tests/ethereum-tests/LegacyTests/Constantinople/VMTests/%: KEVM_MODE     = VMTESTS
 tests/ethereum-tests/LegacyTests/Constantinople/VMTests/%: KEVM_SCHEDULE = DEFAULT
 
-tests/specs/benchmarks/functional-spec%:              KPROVE_FILE   =  functional-spec
-tests/specs/benchmarks/functional-spec%:              KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
-tests/specs/bihu/functional-spec%:                    KPROVE_FILE   =  functional-spec
-tests/specs/bihu/functional-spec%:                    KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
-tests/specs/erc20/functional-spec%:                   KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
-tests/specs/examples/solidity-code-spec%:             KPROVE_EXT    =  md
-tests/specs/examples/solidity-code-spec%:             KPROVE_FILE   =  solidity-code-spec
-tests/specs/examples/erc20-spec%:                     KPROVE_EXT    =  md
-tests/specs/examples/erc20-spec%:                     KPROVE_FILE   =  erc20-spec
-tests/specs/examples/erc721-spec%:                    KPROVE_EXT    =  md
-tests/specs/examples/erc721-spec%:                    KPROVE_FILE   =  erc721-spec
-tests/specs/examples/storage-spec%:                   KPROVE_EXT    =  md
-tests/specs/examples/storage-spec%:                   KPROVE_FILE   =  storage-spec
-tests/specs/examples/sum-to-n-spec%:                  KPROVE_FILE   =  sum-to-n-spec
-tests/specs/examples/sum-to-n-foundry-spec%:          KPROVE_FILE   =  sum-to-n-foundry-spec
-tests/specs/functional/infinite-gas-spec%:            KPROVE_FILE   =  infinite-gas-spec
-tests/specs/functional/evm-int-simplifications-spec%: KPROVE_FILE   =  evm-int-simplifications-spec
-tests/specs/functional/int-simplifications-spec%:     KPROVE_FILE   =  int-simplifications-spec
-tests/specs/functional/lemmas-no-smt-spec%:           KPROVE_FILE   =  lemmas-no-smt-spec
-tests/specs/functional/lemmas-no-smt-spec%:           KPROVE_OPTS   += --haskell-backend-arg="--smt=none"
-tests/specs/functional/lemmas-spec%:                  KPROVE_FILE   =  lemmas-spec
-tests/specs/functional/merkle-spec%:                  KPROVE_FILE   =  merkle-spec
-tests/specs/functional/storageRoot-spec%:             KPROVE_FILE   =  storageRoot-spec
-tests/specs/mcd/functional-spec%:                     KPROVE_FILE   =  functional-spec
-tests/specs/mcd/functional-spec%:                     KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
-tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_EXT    =  md
-tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_FILE   =  evm-optimizations-spec
-tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_MODULE =  EVM-OPTIMIZATIONS-SPEC-LEMMAS
-
 tests/%.run: tests/%
 	$(KEVM) interpret $< $(KEVM_OPTS) $(KRUN_OPTS) --backend $(TEST_CONCRETE_BACKEND)                                  \
 	    --mode $(KEVM_MODE) --schedule $(KEVM_SCHEDULE) --chainid $(KEVM_CHAINID)                                      \
@@ -624,64 +594,15 @@ test-rest-bchain: poetry build-kevm build-llvm
 
 # Proof Tests
 
-prove_specs_dir          := tests/specs
-prove_failing_tests      := $(shell cat tests/failing-symbolic.$(TEST_SYMBOLIC_BACKEND))
-prove_pyk_failing_tests  := $(shell cat tests/failing-symbolic.pyk)
-prove_slow_tests         := $(shell cat tests/slow.$(TEST_SYMBOLIC_BACKEND))
-prove_skip_tests         := $(prove_failing_tests) $(prove_slow_tests)
-prove_benchmarks_tests   := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/benchmarks/*-spec.k))
-prove_functional_tests   := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/functional/*-spec.k))
-prove_opcodes_tests      := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/opcodes/*-spec.k))
-prove_erc20_tests        := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/erc20/*/*-spec.k))
-prove_bihu_tests         := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/bihu/*-spec.k))
-prove_examples_tests     := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/examples/*-spec.k) $(wildcard $(prove_specs_dir)/examples/*-spec.md))
-prove_mcd_tests          := $(filter-out $(prove_skip_tests), $(wildcard $(prove_specs_dir)/mcd/*-spec.k))
-prove_optimization_tests := $(filter-out $(prove_skip_tests), tests/specs/opcodes/evm-optimizations-spec.md)
-prove_all_tests          := $(prove_benchmarks_tests) $(prove_functional_tests) $(prove_opcodes_tests) $(prove_erc20_tests) $(prove_bihu_tests) $(prove_examples_tests) $(prove_mcd_tests) $(prove_optimization_tests)
-prove_pyk_tests          := $(filter-out $(prove_pyk_failing_tests), $(prove_all_tests))
-prove_smoke_tests        := $(shell cat tests/specs/smoke)
+prove_smoke_tests := $(shell cat tests/specs/smoke)
 
-## best-effort list of prove kompiled definitions to produce ahead of time
-prove_haskell_definitions :=                                                              \
-                             tests/specs/benchmarks/functional-spec/haskell/timestamp     \
-                             tests/specs/benchmarks/verification/haskell/timestamp        \
-                             tests/specs/bihu/functional-spec/haskell/timestamp           \
-                             tests/specs/bihu/verification/haskell/timestamp              \
-                             tests/specs/erc20/verification/haskell/timestamp             \
-                             tests/specs/examples/erc20-spec/haskell/timestamp            \
-                             tests/specs/examples/erc721-spec/haskell/timestamp           \
-                             tests/specs/examples/storage-spec/haskell/timestamp          \
-                             tests/specs/examples/solidity-code-spec/haskell/timestamp    \
-                             tests/specs/examples/sum-to-n-spec/haskell/timestamp         \
-                             tests/specs/examples/sum-to-n-foundry-spec/haskell/timestamp \
-                             tests/specs/functional/infinite-gas-spec/haskell/timestamp   \
-                             tests/specs/functional/lemmas-no-smt-spec/haskell/timestamp  \
-                             tests/specs/functional/lemmas-spec/haskell/timestamp         \
-                             tests/specs/functional/merkle-spec/haskell/timestamp         \
-                             tests/specs/functional/storageRoot-spec/haskell/timestamp    \
-                             tests/specs/mcd/functional-spec/haskell/timestamp            \
-                             tests/specs/mcd/verification/haskell/timestamp               \
-                             tests/specs/opcodes/evm-optimizations-spec/haskell/timestamp
-build-prove-haskell: $(KEVM_BIN)/kevm $(prove_haskell_definitions)
-build-prove: $(prove_haskell_definitions)
+test-prove: tests/specs/opcodes/evm-optimizations-spec.md build-kevm poetry
+	$(MAKE) -C kevm-pyk/ test-integration TEST_ARGS+='-k test_prove -n8'
 
-test-prove: test-prove-benchmarks test-prove-functional test-prove-opcodes test-prove-erc20 test-prove-bihu test-prove-examples test-prove-mcd test-prove-optimizations
-test-prove-benchmarks:    $(prove_benchmarks_tests:=.prove)
-test-prove-functional:    $(prove_functional_tests:=.prove)
-test-prove-opcodes:       $(prove_opcodes_tests:=.prove)
-test-prove-erc20:         $(prove_erc20_tests:=.prove)
-test-prove-bihu:          $(prove_bihu_tests:=.prove)
-test-prove-examples:      $(prove_examples_tests:=.prove)
-test-prove-mcd:           $(prove_mcd_tests:=.prove)
-test-prove-optimizations: $(prove_optimization_tests:=.prove)
-test-prove-smoke:         $(prove_smoke_tests:=.prove)
-
-test-failing-prove: $(prove_failing_tests:=.prove)
+test-prove-smoke: $(prove_smoke_tests:=.prove)
 
 test-klab-prove: KPROVE_OPTS += --debugger
 test-klab-prove: $(smoke_tests_prove:=.prove)
-
-$(prove_pyk_tests:=.prove): KPROVE_OPTS += --pyk --max-depth 1000 --verbose
 
 # to generate optimizations.md, run: ./optimizer/optimize.sh &> output
 tests/specs/opcodes/evm-optimizations-spec.md: include/kframework/optimizations.md

.github/workflows/test-pr.yml
@@ -79,7 +79,7 @@ jobs:
     name: 'Build and Test KEVM concrete execution'
     needs: kevm-pyk-code-quality-checks
     runs-on: [self-hosted, linux, normal]
-    timeout-minutes: 20
+    timeout-minutes: 25
     steps:
       - name: 'Check out code'
         uses: actions/checkout@v3
@@ -127,12 +127,8 @@ jobs:
         uses: ./.github/actions/with-docker
         with:
           tag: kevm-ci-haskell-${{ github.sha }}
-      - name: 'Build kevm-pyk'
-        run: docker exec -u user kevm-ci-haskell-${GITHUB_SHA} /bin/bash -c 'make poetry'
-      - name: 'Build Definitions'
-        run: docker exec -u user kevm-ci-haskell-${GITHUB_SHA} /bin/bash -c 'make build-prove-haskell -j4'
       - name: 'Prove Haskell'
-        run: docker exec -u user kevm-ci-haskell-${GITHUB_SHA} /bin/bash -c 'make test-prove -j9 TEST_SYMBOLIC_BACKEND=haskell'
+        run: docker exec -u user kevm-ci-haskell-${GITHUB_SHA} /bin/bash -c 'make test-prove'
       - name: 'Tear down Docker'
         if: always()
         run: |

bin/kevm
@@ -129,20 +129,20 @@ run_prove() {
     check_k_install
 
     if ${pyk}; then
-	run_kevm_pyk prove "${run_file}" "$@"
+        run_kevm_pyk prove "${run_file}" "$@"
     else
-	prove_args=(prove-legacy "${run_file}")
-	! ${debugger} || prove_args+=(--debugger)
-	if ${profile_haskell}; then
-	    eventlog_name="${run_file}.eventlog"
-	    prove_args+=("--haskell-backend-arg=\"+RTS -l -ol${eventlog_name} -RTS\"")
-	    cur_dir=$(pwd)
-	    run_script="cd "${cur_dir}" && run_kevm_pyk "${prove_args[@]}" "$@""
-            timeout -s INT "${profile_timeout}" bash -c "${run_script}" || true
-            execute kore-prof "${eventlog_name}" ${kore_prof_arg} > "${eventlog_name}.json"
-	else
-	    run_kevm_pyk "${prove_args[@]}" "$@"
-	fi
+        prove_args=(prove-legacy "${run_file}")
+        ! ${debugger} || prove_args+=(--debugger)
+        if ${profile_haskell}; then
+            eventlog_name="${run_file}.eventlog"
+            prove_args+=("--haskell-backend-arg=\"+RTS -l -ol${eventlog_name} -RTS\"")
+            cur_dir=$(pwd)
+            run_script="cd "${cur_dir}" && run_kevm_pyk "${prove_args[@]}" "$@""
+                timeout -s INT "${profile_timeout}" bash -c "${run_script}" || true
+                execute kore-prof "${eventlog_name}" ${kore_prof_arg} > "${eventlog_name}.json"
+        else
+            run_kevm_pyk "${prove_args[@]}" "$@"
+        fi
     fi
 }
 

kevm-pyk/pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "poetry.core.masonry.api"
 
 [tool.poetry]
 name = "kevm-pyk"
-version = "1.0.205"
+version = "1.0.206"
 description = ""
 authors = [
     "Runtime Verification, Inc. <contact@runtimeverification.com>",

kevm-pyk/src/kevm_pyk/__main__.py
@@ -197,7 +197,7 @@ def exec_prove_legacy(
     bug_report: bool = False,
     save_directory: Path | None = None,
     spec_module: str | None = None,
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
     debug: bool = False,
     debugger: bool = False,
@@ -248,7 +248,7 @@ def exec_prove(
     bug_report: bool = False,
     save_directory: Path | None = None,
     spec_module: str | None = None,
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
     reinit: bool = False,
     max_depth: int = 1000,
@@ -281,6 +281,9 @@ def exec_prove(
         claim_labels=claim_labels,
         exclude_claim_labels=exclude_claim_labels,
     )
+
+    if not claims:
+        raise ValueError(f'No claims found in file: {spec_file}')
 
     if isinstance(kore_rpc_command, str):
         kore_rpc_command = kore_rpc_command.split()
@@ -367,7 +370,9 @@ def exec_prove(
             if failure_info and failure_log is not None:
                 for line in failure_log:
                     print(line)
-    sys.exit(failed)
+
+    if failed:
+        sys.exit(failed)
 
 
 def exec_prune_proof(
@@ -377,7 +382,7 @@ def exec_prune_proof(
     includes: Iterable[str] = (),
     save_directory: Path | None = None,
     spec_module: str | None = None,
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
     **kwargs: Any,
 ) -> None:
@@ -413,7 +418,7 @@ def exec_show_kcfg(
     spec_file: Path,
     save_directory: Path | None = None,
     includes: Iterable[str] = (),
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
     spec_module: str | None = None,
     md_selector: str | None = None,
@@ -461,7 +466,7 @@ def exec_view_kcfg(
     spec_file: Path,
     save_directory: Path | None = None,
     includes: Iterable[str] = (),
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
     spec_module: str | None = None,
     md_selector: str | None = None,

kevm-pyk/src/kevm_pyk/kompile.py
@@ -130,7 +130,7 @@ def kevm_kompile(
     main_module: str | None,
     syntax_module: str | None,
     includes: Iterable[str] = (),
-    emit_json: bool,
+    emit_json: bool = True,
     read_only: bool = False,
     ccopts: Iterable[str] = (),
     optimization: int = 0,

kevm-pyk/src/kevm_pyk/utils.py
@@ -41,7 +41,7 @@ def get_apr_proof_for_spec(  # noqa: N802
     spec_module_name: str | None = None,
     include_dirs: Iterable[Path] = (),
     md_selector: str | None = None,
-    claim_labels: Iterable[str] = (),
+    claim_labels: Iterable[str] | None = None,
     exclude_claim_labels: Iterable[str] = (),
 ) -> APRProof:
     if save_directory is None:

kevm-pyk/src/tests/integration/test_prove.py
@@ -1,0 +1,243 @@
+from __future__ import annotations
+
+import logging
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TYPE_CHECKING, final
+
+import pytest
+from pyk.prelude.ml import mlTop
+
+from kevm_pyk import config
+from kevm_pyk.__main__ import exec_prove
+from kevm_pyk.kevm import KEVM
+from kevm_pyk.kompile import KompileTarget, kevm_kompile
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from typing import Any, Final
+
+    from pytest import LogCaptureFixture, TempPathFactory
+
+
+sys.setrecursionlimit(10**8)
+
+REPO_ROOT: Final = Path(__file__).parents[4]
+TEST_DIR: Final = REPO_ROOT / 'tests'
+SPEC_DIR: Final = TEST_DIR / 'specs'
+
+
+# -------------------
+# Test specifications
+# -------------------
+
+
+def spec_files(dir_name: str, glob: str) -> tuple[Path, ...]:
+    test_dir = SPEC_DIR / dir_name
+    res = tuple(test_dir.glob(glob))
+    assert res
+    return res
+
+
+BENCHMARK_TESTS: Final = spec_files('benchmarks', '*-spec.k')
+FUNCTIONAL_TESTS: Final = spec_files('functional', '*-spec.k')
+OPCODES_TESTS: Final = spec_files('opcodes', '*-spec.k')
+ERC20_TESTS: Final = spec_files('erc20', '*/*-spec.k')
+BIHU_TESTS: Final = spec_files('bihu', '*-spec.k')
+EXAMPLES_TESTS: Final = spec_files('examples', '*-spec.k') + spec_files('examples', '*-spec.md')
+MCD_TESTS: Final = spec_files('mcd', '*-spec.k')
+OPTIMIZATION_TESTS: Final = (SPEC_DIR / 'opcodes/evm-optimizations-spec.md',)
+
+ALL_TESTS: Final = sum(
+    [
+        BENCHMARK_TESTS,
+        FUNCTIONAL_TESTS,
+        OPCODES_TESTS,
+        ERC20_TESTS,
+        BIHU_TESTS,
+        EXAMPLES_TESTS,
+        MCD_TESTS,
+        OPTIMIZATION_TESTS,
+    ],
+    (),
+)
+
+
+def exclude_list(exclude_file: Path) -> list[Path]:
+    res = [REPO_ROOT / test_path for test_path in exclude_file.read_text().splitlines()]
+    assert res
+    return res
+
+
+FAILING_PYK_TESTS: Final = exclude_list(TEST_DIR / 'failing-symbolic.pyk')
+SLOW_TESTS: Final = exclude_list(TEST_DIR / 'slow.haskell')
+FAILING_TESTS: Final = exclude_list(TEST_DIR / 'failing-symbolic.haskell')
+
+
+# -----------
+# Kompilation
+# -----------
+
+
+KOMPILE_MAIN_FILE: Final = {
+    'benchmarks/functional-spec.k': 'functional-spec.k',
+    'bihu/functional-spec.k': 'functional-spec.k',
+    'examples/solidity-code-spec.md': 'solidity-code-spec.md',
+    'examples/erc20-spec.md': 'erc20-spec.md',
+    'examples/erc721-spec.md': 'erc721-spec.md',
+    'examples/storage-spec.k': 'storage-spec.k',
+    'examples/sum-to-n-spec.k': 'sum-to-n-spec.k',
+    'examples/sum-to-n-foundry-spec.k': 'sum-to-n-foundry-spec.k',
+    'functional/infinite-gas-spec.k': 'infinite-gas-spec.k',
+    'functional/evm-int-simplifications-spec.k': 'evm-int-simplifications-spec.k',
+    'functional/int-simplifications-spec.k': 'int-simplifications-spec.k',
+    'functional/lemmas-no-smt-spec.k': 'lemmas-no-smt-spec.k',
+    'functional/lemmas-spec.k': 'lemmas-spec.k',
+    'functional/merkle-spec.k': 'merkle-spec.k',
+    'functional/storageRoot-spec.k': 'storageRoot-spec.k',
+    'mcd/functional-spec.k': 'functional-spec.k',
+    'opcodes/evm-optimizations-spec.md': 'evm-optimizations-spec.md',
+}
+
+KOMPILE_MAIN_MODULE: Final = {
+    'benchmarks/functional-spec.k': 'FUNCTIONAL-SPEC-SYNTAX',
+    'bihu/functional-spec.k': 'FUNCTIONAL-SPEC-SYNTAX',
+    'erc20/functional-spec.k': 'FUNCTIONAL-SPEC-SYNTAX',
+    'mcd/functional-spec.k': 'FUNCTIONAL-SPEC-SYNTAX',
+    'opcodes/evm-optimizations-spec.md': 'EVM-OPTIMIZATIONS-SPEC-LEMMAS',
+}
+
+
+@final
+@dataclass(frozen=True)
+class Target:
+    main_file: Path
+    main_module_name: str
+
+    def __call__(self, output_dir: Path) -> Path:
+        return kevm_kompile(
+            output_dir=output_dir,
+            target=KompileTarget.HASKELL,
+            main_file=self.main_file,
+            main_module=self.main_module_name,
+            syntax_module=self.main_module_name,
+            debug=True,
+        )
+
+
+@pytest.fixture(scope='module')
+def definition_dir_for(tmp_path_factory: TempPathFactory) -> Callable[[Path], Path]:
+    cache_dir = tmp_path_factory.mktemp('kompiled')
+    cache: dict[Target, Path] = {}
+
+    def kompile(spec_file: Path) -> Path:
+        target = _target_for_spec(spec_file)
+
+        if target not in cache:
+            output_dir_name = f'{target.main_file.stem}-kompiled-{len(cache)}'
+            cache[target] = target(output_dir=cache_dir / output_dir_name)
+
+        return cache[target]
+
+    return kompile
+
+
+def _target_for_spec(spec_file: Path) -> Target:
+    spec_file = spec_file.resolve()
+    spec_id = str(spec_file.relative_to(SPEC_DIR))
+    spec_root = SPEC_DIR / spec_file.relative_to(SPEC_DIR).parents[-2]
+    main_file = spec_root / KOMPILE_MAIN_FILE.get(spec_id, 'verification.k')
+    main_module_name = KOMPILE_MAIN_MODULE.get(spec_id, 'VERIFICATION')
+    return Target(main_file, main_module_name)
+
+
+# ---------
+# Pyk tests
+# ---------
+
+
+SKIPPED_PYK_TESTS: Final = set().union(SLOW_TESTS, FAILING_TESTS, FAILING_PYK_TESTS)
+
+
+@pytest.mark.parametrize(
+    'spec_file',
+    ALL_TESTS,
+    ids=[str(spec_file.relative_to(SPEC_DIR)) for spec_file in ALL_TESTS],
+)
+def test_pyk_prove(
+    spec_file: Path,
+    definition_dir_for: Callable[[Path], Path],
+    tmp_path: Path,
+) -> None:
+    if spec_file in SKIPPED_PYK_TESTS:
+        pytest.skip()
+
+    # Given
+    use_directory = tmp_path / 'kprove'
+    use_directory.mkdir()
+
+    # When
+    definition_dir = definition_dir_for(spec_file)
+    exec_prove(
+        spec_file=spec_file,
+        definition_dir=definition_dir,
+        includes=[str(config.INCLUDE_DIR)],
+        save_directory=use_directory,
+        smt_timeout=125,
+        smt_retry_limit=4,
+        md_selector='foo',  # TODO Ignored flag, this is to avoid KeyError
+    )
+
+
+# ------------
+# Legacy tests
+# ------------
+
+
+SKIPPED_LEGACY_TESTS: Final = set().union(SLOW_TESTS, FAILING_TESTS)
+
+
+PROVE_ARGS: Final[dict[str, Any]] = {
+    'functional/lemmas-no-smt-spec.k': {
+        'haskell_args': ['--smt=none'],
+    },
+}
+
+
+@pytest.mark.parametrize(
+    'spec_file',
+    FAILING_PYK_TESTS,
+    ids=[str(spec_file.relative_to(SPEC_DIR)) for spec_file in FAILING_PYK_TESTS],
+)
+def test_legacy_prove(
+    spec_file: Path,
+    definition_dir_for: Callable[[Path], Path],
+    tmp_path: Path,
+    caplog: LogCaptureFixture,
+) -> None:
+    caplog.set_level(logging.INFO)
+
+    if spec_file in SKIPPED_LEGACY_TESTS:
+        pytest.skip()
+
+    # Given
+    spec_id = str(spec_file.relative_to(SPEC_DIR))
+    args = PROVE_ARGS.get(spec_id, {})
+
+    log_file = tmp_path / 'log.txt'
+    use_directory = tmp_path / 'kprove'
+    use_directory.mkdir()
+
+    # When
+    try:
+        definition_dir = definition_dir_for(spec_file)
+        kevm = KEVM(definition_dir, use_directory=use_directory)
+        actual = kevm.prove(spec_file=spec_file, include_dirs=[config.INCLUDE_DIR], **args)
+    except BaseException:
+        raise
+    finally:
+        log_file.write_text(caplog.text)
+
+    # Then
+    assert actual == mlTop('K')

commit 0bc354bd29a30a9c9a671e1d050df228a8604d86

HOTFIX restore the ability to run single prover tests
This was broken by https://github.com/runtimeverification/evm-semantics/pull/1894
and is necessary for test update automation and a custom CI workflow.

Makefile
@@ -409,6 +409,36 @@ test: test-conformance test-prove test-interactive test-parse test-kevm-pyk
 tests/ethereum-tests/LegacyTests/Constantinople/VMTests/%: KEVM_MODE     = VMTESTS
 tests/ethereum-tests/LegacyTests/Constantinople/VMTests/%: KEVM_SCHEDULE = DEFAULT
 
+# Fix-up for running single prover tests with make
+tests/specs/benchmarks/functional-spec%:              KPROVE_FILE   =  functional-spec
+tests/specs/benchmarks/functional-spec%:              KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
+tests/specs/bihu/functional-spec%:                    KPROVE_FILE   =  functional-spec
+tests/specs/bihu/functional-spec%:                    KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
+tests/specs/erc20/functional-spec%:                   KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
+tests/specs/examples/solidity-code-spec%:             KPROVE_EXT    =  md
+tests/specs/examples/solidity-code-spec%:             KPROVE_FILE   =  solidity-code-spec
+tests/specs/examples/erc20-spec%:                     KPROVE_EXT    =  md
+tests/specs/examples/erc20-spec%:                     KPROVE_FILE   =  erc20-spec
+tests/specs/examples/erc721-spec%:                    KPROVE_EXT    =  md
+tests/specs/examples/erc721-spec%:                    KPROVE_FILE   =  erc721-spec
+tests/specs/examples/storage-spec%:                   KPROVE_EXT    =  md
+tests/specs/examples/storage-spec%:                   KPROVE_FILE   =  storage-spec
+tests/specs/examples/sum-to-n-spec%:                  KPROVE_FILE   =  sum-to-n-spec
+tests/specs/examples/sum-to-n-foundry-spec%:          KPROVE_FILE   =  sum-to-n-foundry-spec
+tests/specs/functional/infinite-gas-spec%:            KPROVE_FILE   =  infinite-gas-spec
+tests/specs/functional/evm-int-simplifications-spec%: KPROVE_FILE   =  evm-int-simplifications-spec
+tests/specs/functional/int-simplifications-spec%:     KPROVE_FILE   =  int-simplifications-spec
+tests/specs/functional/lemmas-no-smt-spec%:           KPROVE_FILE   =  lemmas-no-smt-spec
+tests/specs/functional/lemmas-no-smt-spec%:           KPROVE_OPTS   += --haskell-backend-arg="--smt=none"
+tests/specs/functional/lemmas-spec%:                  KPROVE_FILE   =  lemmas-spec
+tests/specs/functional/merkle-spec%:                  KPROVE_FILE   =  merkle-spec
+tests/specs/functional/storageRoot-spec%:             KPROVE_FILE   =  storageRoot-spec
+tests/specs/mcd/functional-spec%:                     KPROVE_FILE   =  functional-spec
+tests/specs/mcd/functional-spec%:                     KPROVE_MODULE =  FUNCTIONAL-SPEC-SYNTAX
+tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_EXT    =  md
+tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_FILE   =  evm-optimizations-spec
+tests/specs/opcodes/evm-optimizations-spec%:          KPROVE_MODULE =  EVM-OPTIMIZATIONS-SPEC-LEMMAS
+
 tests/%.run: tests/%
 	$(KEVM) interpret $< $(KEVM_OPTS) $(KRUN_OPTS) --backend $(TEST_CONCRETE_BACKEND)                                  \
 	    --mode $(KEVM_MODE) --schedule $(KEVM_SCHEDULE) --chainid $(KEVM_CHAINID)                                      \