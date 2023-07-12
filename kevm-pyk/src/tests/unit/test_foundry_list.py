6a3c6b73867fe396b1dbbd41dd8c12c0bc0dae7f
François Guyot <francois.guyot@runtimeverification.com>
2023-07-12 10:04:59 +0000

    fix linting checks

--- kevm-pyk/src/tests/unit/test_foundry_list.py
+++ kevm-pyk/src/tests/unit/test_foundry_list.py
@@ -4,9 +4,9 @@ import json
 from typing import TYPE_CHECKING
 
 import pytest
-from tests.utils import REPO_ROOT
-
 from pyk.proof import APRProof
+
+from tests.utils import REPO_ROOT
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -23,6 +23,7 @@ class APRProofOutput:
     def from_str(self, list_out: str) -> list[str]:
         return list_out.split(':')
 
+
 with FOUNDRY_LIST_PATH.open() as f:
     foundry_list_expected = f.read()
 assert foundry_list_expected
@@ -30,15 +31,18 @@ assert foundry_list_expected
 lists = foundry_list_expected.split('\n\n')
 assert len(lists) == len(APR_PROOFS)
 
-list_outputs = {list_output.split('\n')[0].split(': ')[1]: list_output.rstrip('\n') for list_output in lists }
+list_outputs = {list_output.split('\n')[0].split(': ')[1]: list_output.rstrip('\n') for list_output in lists}
 
-@pytest.mark.parametrize('apr_proof', APR_PROOFS, ids=[str(apr_proof.relative_to(TEST_DATA)) for apr_proof in APR_PROOFS])
+
+@pytest.mark.parametrize(
+    'apr_proof', APR_PROOFS, ids=[str(apr_proof.relative_to(TEST_DATA)) for apr_proof in APR_PROOFS]
+)
 def test_foundry_list(apr_proof: Path) -> None:
     with apr_proof.open() as f:
         raw_proof = json.load(f)
 
     proof = APRProof.from_dict(raw_proof)
-    summary = '\n'.join([comp for comp in proof.summary])
+    summary = '\n'.join([comp for comp in proof.summary])  # noqa: C416
 
     list_out = list_outputs.get(proof.id)
     assert list_out is not None
