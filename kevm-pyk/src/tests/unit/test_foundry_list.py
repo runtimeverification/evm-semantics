eb299f350878f3b817220e54a0bf71618fda6e54
François Guyot <francois.guyot@runtimeverification.com>
2023-07-12 09:40:48 +0000

    add APRProof summary test

--- kevm-pyk/src/tests/unit/test_foundry_list.py
+++ kevm-pyk/src/tests/unit/test_foundry_list.py
@@ -18,6 +18,11 @@ FOUNDRY_LIST_PATH: Final = TEST_DATA / 'foundry-list.expected'
 APR_PROOFS: Final = tuple((TEST_DATA / 'apr_proofs').glob('*.json'))
 assert APR_PROOFS
 
+
+class APRProofOutput:
+    def from_str(self, list_out: str) -> list[str]:
+        return list_out.split(':')
+
 with FOUNDRY_LIST_PATH.open() as f:
     foundry_list_expected = f.read()
 assert foundry_list_expected
@@ -25,8 +30,7 @@ assert foundry_list_expected
 lists = foundry_list_expected.split('\n\n')
 assert len(lists) == len(APR_PROOFS)
 
-list_ids = set([list_output.split('\n')[0].split(': ')[1] for list_output in lists])
-assert len(list_ids) == len(APR_PROOFS)
+list_outputs = {list_output.split('\n')[0].split(': ')[1]: list_output.rstrip('\n') for list_output in lists }
 
 @pytest.mark.parametrize('apr_proof', APR_PROOFS, ids=[str(apr_proof.relative_to(TEST_DATA)) for apr_proof in APR_PROOFS])
 def test_foundry_list(apr_proof: Path) -> None:
@@ -34,7 +38,9 @@ def test_foundry_list(apr_proof: Path) -> None:
         raw_proof = json.load(f)
 
     proof = APRProof.from_dict(raw_proof)
+    summary = '\n'.join([comp for comp in proof.summary])
 
-    assert proof.id in list_ids
+    list_out = list_outputs.get(proof.id)
+    assert list_out is not None
 
-    assert True
+    assert summary == list_out
