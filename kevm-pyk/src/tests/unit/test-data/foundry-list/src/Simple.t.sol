fb1bdeeb647d4b9e5cf7327e9d1c5109fb07d0ce
François Guyot <francois.guyot@runtimeverification.com>
2023-07-13 10:13:36 +0000

    apply foundry-list direct output unit test

---
+++ kevm-pyk/src/tests/unit/test-data/foundry-list/src/Simple.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.13;
+
+import "forge-std/Test.sol";
+
+contract AssertTest is Test {
+    function setUp() public {}
+
+    function test_assert_true() public pure {
+        assert(true);
+    }
+
+    function test_assert_false() public pure {
+        assert(false);
+    }
+
+    function testFail_assert_true() public pure {
+        assert(true);
+    }
+
+    function testFail_assert_false() public pure {
+         assert(false);
+     }
+}
--- kevm-pyk/src/tests/unit/test_foundry_list.py
+++ kevm-pyk/src/tests/unit/test_foundry_list.py
@@ -1,50 +1,26 @@
 from __future__ import annotations
 
-import json
 from typing import TYPE_CHECKING
+from kevm_pyk.foundry import foundry_list
 
-import pytest
-from pyk.proof import APRProof
-
-from tests.utils import REPO_ROOT
+from tests.unit.utils import TEST_DATA_DIR
 
 if TYPE_CHECKING:
-    from pathlib import Path
     from typing import Final
 
-TEST_DATA = REPO_ROOT / 'kevm-pyk/src/tests/unit/test-data'
 
-FOUNDRY_LIST_PATH: Final = TEST_DATA / 'foundry-list.expected'
-APR_PROOFS: Final = tuple((TEST_DATA / 'apr_proofs').glob('*.json'))
-assert APR_PROOFS
+LIST_DATA_DIR: Final = TEST_DATA_DIR / 'foundry-list'
+LIST_EXPECTED: Final = LIST_DATA_DIR / 'foundry-list.expected'
 
 
-class APRProofOutput:
-    def from_str(self, list_out: str) -> list[str]:
-        return list_out.split(':')
+def test_foundry_list(update_expected_output: bool) -> None:
+    with LIST_EXPECTED.open() as f:
+        foundry_list_expected = f.read().rstrip()
+    assert foundry_list_expected
 
+    list_out = '\n'.join(foundry_list(LIST_DATA_DIR))
 
-with FOUNDRY_LIST_PATH.open() as f:
-    foundry_list_expected = f.read()
-assert foundry_list_expected
-
-lists = foundry_list_expected.split('\n\n')
-assert len(lists) == len(APR_PROOFS)
-
-list_outputs = {list_output.split('\n')[0].split(': ')[1]: list_output.rstrip('\n') for list_output in lists}
-
-
-@pytest.mark.parametrize(
-    'apr_proof', APR_PROOFS, ids=[str(apr_proof.relative_to(TEST_DATA)) for apr_proof in APR_PROOFS]
-)
-def test_foundry_list(apr_proof: Path) -> None:
-    with apr_proof.open() as f:
-        raw_proof = json.load(f)
-
-    proof = APRProof.from_dict(raw_proof)
-    summary = '\n'.join([comp for comp in proof.summary])  # noqa: C416
-
-    list_out = list_outputs.get(proof.id)
-    assert list_out is not None
-
-    assert summary == list_out
+    if update_expected_output:
+        LIST_EXPECTED.write_text(list_out)
+    else:
+        assert list_out == foundry_list_expected
