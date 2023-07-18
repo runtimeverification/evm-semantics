6041e2b97fcec7e26715bbaf965f74cd5c0721a4
François Guyot <francois.guyot@runtimeverification.com>
2023-07-18 09:37:12 +0000

    cleanup of unused files

--- kevm-pyk/src/tests/unit/test_foundry_list.py
+++ kevm-pyk/src/tests/unit/test_foundry_list.py
@@ -1,17 +1,12 @@
 from __future__ import annotations
 
-from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
-
-from pyk.utils import run_process
 
 from kevm_pyk.foundry import foundry_list
 from tests.unit.utils import TEST_DATA_DIR
 
 if TYPE_CHECKING:
     from typing import Final
-
-FORGE_STD_REF: Final = '27e14b7'
 
 LIST_DATA_DIR: Final = TEST_DATA_DIR / 'foundry-list'
 LIST_EXPECTED: Final = LIST_DATA_DIR / 'foundry-list.expected'
@@ -22,12 +17,6 @@ def test_foundry_list(update_expected_output: bool) -> None:
         foundry_list_expected = f.read().rstrip()
     assert foundry_list_expected
 
-    try:
-        run_process(['forge', 'install', '--no-git', f'foundry-rs/forge-std@{FORGE_STD_REF}'], cwd=LIST_DATA_DIR)
-    except CalledProcessError:
-        pass
-
-    run_process(['forge', 'build'], cwd=LIST_DATA_DIR)
     list_out = '\n'.join(foundry_list(LIST_DATA_DIR))
 
     if update_expected_output:
