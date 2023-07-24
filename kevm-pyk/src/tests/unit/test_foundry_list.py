0156f05f1de4129515c2cdc145ea3cb9fd24eae5
franfran <fgg67@hotmail.fr>
2023-07-25 01:10:25 +0300

    Revert "remove env dependency"
    This reverts commit 513acc1239ce6146b65db1eaac2166ab977903b5.

--- kevm-pyk/src/tests/unit/test_foundry_list.py
+++ kevm-pyk/src/tests/unit/test_foundry_list.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-# from kevm_pyk.foundry import foundry_list
+from kevm_pyk.foundry import foundry_list
 from tests.unit.utils import TEST_DATA_DIR
 
 if TYPE_CHECKING:
@@ -17,9 +17,9 @@ def test_foundry_list(update_expected_output: bool) -> None:
         foundry_list_expected = f.read().rstrip()
     assert foundry_list_expected
 
-    # list_out = '\n'.join(foundry_list(LIST_DATA_DIR))
+    list_out = '\n'.join(foundry_list(LIST_DATA_DIR))
 
-    # if update_expected_output:
-    #     LIST_EXPECTED.write_text(list_out)
-    # else:
-    #     assert list_out == foundry_list_expected
+    if update_expected_output:
+        LIST_EXPECTED.write_text(list_out)
+    else:
+        assert list_out == foundry_list_expected
