f229413933c58b8111c3892471abb41054261167
Jan Tusil <jenda.tusil@gmail.com>
2023-08-09 12:11:55 +0200

    remove old testing file

--- test-frob.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-KEVM_PREFIX=/nix/store/z30l75z7l3m2nynf52n13cwqz2586rb1-kevm-8ed6754edb8e7e8cadbcaa1fa08f255241dd34b6/
-echo 'Kompiling'
-kevm-pyk kompile --read-only-kompiled-directory --target haskell --definition "$KEVM_PREFIX/lib/kevm/haskell" --syntax-module VERIFICATION-SYNTAX --main-module VERIFICATION ./tests/specs/mcd/vat-frob-diff-zero-dart-pass-rough-reuse-spec.k -o frob-out.d
-
-
-echo 'Proving'
-kevm-pyk prove --verbose --workers 4 --smt-timeout 1000 --smt-retry-limit 5 --max-depth 100 --definition frob-out.d/ -I include/kframework/ -I "$KEVM_PREFIX/lib/kevm/include/kframework/" ./tests/specs/mcd/vat-frob-diff-zero-dart-pass-rough-reuse-spec.k --save-directory ./frob-save.d
