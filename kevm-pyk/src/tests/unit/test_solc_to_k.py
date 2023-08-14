1e9b175bd9ab991d5c24f6c422f8018cbc7087c3
franfran <fgg67@hotmail.fr>
2023-08-14 14:20:51 +0300

    add solc_to_k unit test

---
+++ kevm-pyk/src/tests/unit/test_solc_to_k.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+from pyk.kast.inner import KToken, KVariable
+
+from kevm_pyk.kevm import KEVM
+from kevm_pyk.solc_to_k import _range_predicate
+
+from .utils import TEST_DATA_DIR
+
+if TYPE_CHECKING:
+    from typing import Final
+
+    from pyk.kast.inner import KInner
+
+
+EXAMPLES_DIR: Final = TEST_DATA_DIR / 'examples'
+
+TEST_DATA: list[tuple[str, KInner, str, KInner | None]] = [
+    ('bytes4', KVariable('V0_x'), 'bytes4', KEVM.range_bytes(KToken('4', 'Int'), KVariable('V0_x'))),
+    ('int128', KVariable('V0_x'), 'int128', KEVM.range_sint(128, KVariable('V0_x'))),
+    ('int24', KVariable('V0_x'), 'int24', KEVM.range_sint(24, KVariable('V0_x'))),
+]
+
+
+@pytest.mark.parametrize(
+    'test_id,term,type,expected',
+    TEST_DATA,
+    ids=[test_id for test_id, *_ in TEST_DATA],
+)
+def test_range_predicate(test_id: str, term: KInner, type: str, expected: KInner | None) -> None:
+    # When
+    ret = _range_predicate(term, type)
+
+    # Then
+    assert ret == expected
