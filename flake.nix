17f22705db14082fda487e909164d4a1a48c0be7
rv-jenkins <admin@runtimeverification.com>
2023-08-22 09:23:26 -0600

    Update dependency: deps/pyk_release (#2027)
    * deps/pyk_release: Set Version v0.1.422
    
    * Set Version: 1.0.272
    
    * kevm-pyk/: sync poetry files pyk version v0.1.422
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     ethereum-legacytests.url = "github:ethereum/legacytests/d7abc42a7b352a7b44b1f66b58aca54e4af6a9d7";
     ethereum-legacytests.flake = false;
     haskell-backend.follows = "k-framework/haskell-backend";
-    pyk.url = "github:runtimeverification/pyk/v0.1.421";
+    pyk.url = "github:runtimeverification/pyk/v0.1.422";
     pyk.inputs.flake-utils.follows = "k-framework/flake-utils";
     pyk.inputs.nixpkgs.follows = "k-framework/nixpkgs";
     foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
