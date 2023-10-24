c28c0b2fad24cfdc4105ac5f17f225161580655a
Jost Berthold <jost.berthold@gmail.com>
2023-10-24 14:12:44 +1100

    limit max_depth to 300 in pyk_prove tests (workaround)

--- kevm-pyk/src/tests/integration/test_prove.py
+++ kevm-pyk/src/tests/integration/test_prove.py
@@ -282,6 +282,7 @@ def test_pyk_prove(
             definition_dir=target.definition_dir,
             includes=[str(include_dir) for include_dir in config.INCLUDE_DIRS] + target.includes,
             save_directory=use_directory,
+            max_depth=300,  # Workaround for ecrecover00-siginvalid issue
             smt_timeout=300,
             smt_retry_limit=10,
             md_selector='foo',  # TODO Ignored flag, this is to avoid KeyError
