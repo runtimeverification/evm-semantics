3540f86e52fc7ab7d67077e8e1b085be14041112
rv-jenkins <admin@runtimeverification.com>
2023-11-30 15:24:57 -0700

    Update dependency: deps/pyk_release (#2197)
    * deps/pyk_release: Set Version v0.1.516
    
    * kevm-pyk/: sync poetry files pyk version v0.1.516
    
    * deps/k_release: sync release file version 6.1.30
    
    * flake.{nix,lock}: update Nix derivations
    
    * Set Version: 1.0.376
    
    * Revert "Remove some runlemma => donelemma style claims (#2080)"
    
    This reverts commit 40854311d0ea9374a0b207d9185a0a35ea013fd5.
    
    * Set Version: 1.0.378
    
    * kevm-pyk/: sync poetry files pyk version v0.1.516
    
    * Revert "Use EqualityProof for functional claims (#1860)"
    
    This reverts commit c9226d7707658ff4e2e3ab5879b1e1f309b2332d.
    
    * deps/pyk_release: Set Version v0.1.518
    
    * kevm-pyk/: sync poetry files pyk version v0.1.518
    
    * deps/k_release: sync release file version 6.1.31
    
    * flake.{nix,lock}: update Nix derivations
    
    * tests/specs/examples/storage-spec.md: typo
    
    * deps/pyk_release: Set Version v0.1.519
    
    * kevm-pyk/: sync poetry files pyk version v0.1.519
    
    * deps/k_release: sync release file version 6.1.32
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/pyk_release: Set Version v0.1.520
    
    * kevm-pyk/: sync poetry files pyk version v0.1.520
    
    * flake.{nix,lock}: update Nix derivations
    
    * deps/pyk_release: Set Version v0.1.521
    
    * kevm-pyk/: sync poetry files pyk version v0.1.521
    
    * deps/k_release: sync release file version 6.1.33
    
    * flake.{nix,lock}: update Nix derivations
    
    * tests/specs/functional/infinite-gas-spec.k: fix typo
    
    * deps/pyk_release: Set Version v0.1.522
    
    * kevm-pyk/: sync poetry files pyk version v0.1.522
    
    * deps/k_release: sync release file version 6.1.34
    
    * flake.{nix,lock}: update Nix derivations
    
    * tests/specs/functional/infinite-gas-spec.k: typo
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "A flake for the KEVM Semantics";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v6.1.26";
+    k-framework.url = "github:runtimeverification/k/v6.1.34";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/pyk/v0.1.515";
+    pyk.url = "github:runtimeverification/pyk/v0.1.522";
     nixpkgs-pyk.follows = "pyk/nixpkgs";
     poetry2nix.follows = "pyk/poetry2nix";
     blockchain-k-plugin = {
