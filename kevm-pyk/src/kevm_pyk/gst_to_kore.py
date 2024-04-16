5a23f8c7bd2f9e54e673d09e91acf1c128b1f5f9
Dwight Guth <dwight.guth@runtimeverification.com>
2024-04-16 11:20:36 -0500

    fix unused imports

--- kevm-pyk/src/kevm_pyk/gst_to_kore.py
+++ kevm-pyk/src/kevm_pyk/gst_to_kore.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 import logging
 import sys
 from argparse import ArgumentParser
--- kevm-pyk/src/tests/integration/test_conformance.py
+++ kevm-pyk/src/tests/integration/test_conformance.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 import sys
 from typing import TYPE_CHECKING
 
