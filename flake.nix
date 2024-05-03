0caa7378bc0d234dd9636d5fea4e34a519f7907e
devops <devops@runtimeverification.com>
2024-05-03 14:37:37 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "A flake for the KEVM Semantics";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.0.40";
+    k-framework.url = "github:runtimeverification/k/v7.0.41";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
-    pyk.url = "github:runtimeverification/k/v7.0.40?dir=pyk";
+    pyk.url = "github:runtimeverification/k/v7.0.41?dir=pyk";
     nixpkgs-pyk.follows = "pyk/nixpkgs";
     poetry2nix.follows = "pyk/poetry2nix";
     blockchain-k-plugin = {
