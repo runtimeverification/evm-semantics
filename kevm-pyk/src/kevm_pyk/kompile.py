5bf3c15d2209c25a22c3f6f706e2c6431c963602
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-05-15 13:17:32 +0000

    Use `kompile` for the `HASKELL` target

--- kevm-pyk/src/kevm_pyk/kompile.py
+++ kevm-pyk/src/kevm_pyk/kompile.py
@@ -8,7 +8,8 @@ from typing import TYPE_CHECKING
 
 from pyk.kdist import kdist
 from pyk.ktool import TypeInferenceMode
-from pyk.ktool.kompile import HaskellKompile, KompileArgs, LLVMKompile, LLVMKompileType, MaudeKompile
+from pyk.ktool.kompile import HaskellKompile, KompileArgs, LLVMKompile, MaudeKompile, PykBackend
+from pyk.ktool.kompile import kompile as pyk_kompile
 from pyk.utils import run_process
 
 from . import config
@@ -18,7 +19,7 @@ if TYPE_CHECKING:
     from pathlib import Path
     from typing import Final
 
-    from pyk.ktool.kompile import Kompile
+    from pyk.ktool.kompile import Kompile, LLVMKompileType
 
 
 _LOGGER: Final = logging.getLogger(__name__)
@@ -168,7 +169,13 @@ def run_kompile(
                 return output_dir
 
             case KompileTarget.HASKELL:
-                base_args_llvm = KompileArgs(
+                return pyk_kompile(
+                    backend=PykBackend.BOOSTER,
+                    output_dir=output_dir,
+                    debug=debug,
+                    verbose=verbose,
+                    type_inference_mode=type_inference_mode,
+                    # ---
                     main_file=main_file,
                     main_module=main_module,
                     syntax_module=syntax_module,
@@ -177,34 +184,12 @@ def run_kompile(
                     hook_namespaces=HOOK_NAMESPACES,
                     emit_json=emit_json,
                     read_only=read_only,
+                    # --- LLVM ---
+                    ccopts=ccopts,
+                    opt_level=optimization,
+                    # --- Haskell ---
+                    haskell_binary=haskell_binary,
                 )
-                kompile_llvm = LLVMKompile(
-                    base_args=base_args_llvm, ccopts=ccopts, opt_level=optimization, llvm_kompile_type=LLVMKompileType.C
-                )
-                kompile_haskell = HaskellKompile(base_args=base_args, haskell_binary=haskell_binary)
-
-                def _kompile_llvm() -> None:
-                    kompile_llvm(
-                        output_dir=output_dir / 'llvm-library',
-                        debug=debug,
-                        verbose=verbose,
-                        type_inference_mode=type_inference_mode,
-                    )
-
-                def _kompile_haskell() -> None:
-                    kompile_haskell(
-                        output_dir=output_dir, debug=debug, verbose=verbose, type_inference_mode=type_inference_mode
-                    )
-
-                with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-                    futures = [
-                        executor.submit(_kompile_llvm),
-                        executor.submit(_kompile_haskell),
-                    ]
-                    [future.result() for future in futures]
-
-                return output_dir
-
             case _:
                 raise ValueError(f'Unsupported target: {target.value}')
 
