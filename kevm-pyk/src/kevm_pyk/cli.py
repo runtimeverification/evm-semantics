8e53e95e06e4fd3e221541901422473f9adb83e4
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-06-10 22:35:44 +0300

    cli.py: --break-on-load-program => --symbolic-immutables

--- kevm-pyk/src/kevm_pyk/cli.py
+++ kevm-pyk/src/kevm_pyk/cli.py
@@ -1122,11 +1122,11 @@ class KEVMCLIArgs(KCLIArgs):
             help='Store a node for every EVM basic block (implies --break-on-calls).',
         )
         args.add_argument(
-            '--break-on-load-program',
+            '--symbolic-immutables',
             dest='break_on_load_program',
             default=None,
             action='store_true',
-            help='Store a node every time the jumpDests are computed, allowing the custom step to apply.',
+            help='Enable support for symbolic immutables in Solidity constructor code.',
         )
         args.add_argument(
             '--max-depth',
