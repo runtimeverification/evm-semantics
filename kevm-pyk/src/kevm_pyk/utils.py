0fd99c025a2d92bb2db066cbfd062e9248e726c6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-06-10 15:53:42 +0000

    kevm-pyk/{utils,__main__}: factor out initialization of APRProofs from main module

--- kevm-pyk/src/kevm_pyk/utils.py
+++ kevm-pyk/src/kevm_pyk/utils.py
@@ -21,6 +21,7 @@ from pyk.kast.manip import (
 from pyk.kcfg import KCFGExplore
 from pyk.kore.rpc import KoreClient, KoreExecLogFormat, TransportType, kore_server
 from pyk.ktool import TypeInferenceMode
+from pyk.prelude.ml import is_bottom, is_top
 from pyk.proof import APRProof, APRProver
 from pyk.proof.implies import EqualityProof, ImpliesProver
 from pyk.proof.proof import parallel_advance_proof
@@ -391,6 +392,26 @@ def constraints_for(vars: list[str], constraints: Iterable[KInner]) -> Iterable[
                 vars.extend(free_vars(constraint))
                 constraints_changed = True
     return accounts_constraints
+
+
+def initialize_apr_proof(cterm_symbolic: CTermSymbolic, proof: APRProof) -> None:
+    init_cterm = proof.kcfg.node(proof.init).cterm
+    target_cterm = proof.kcfg.node(proof.target).cterm
+
+    _LOGGER.info(f'Computing definedness constraint for initial node: {proof.id}')
+    init_cterm = cterm_symbolic.assume_defined(init_cterm)
+
+    _LOGGER.info(f'Simplifying initial and target node: {proof.id}')
+    init_cterm, _ = cterm_symbolic.simplify(init_cterm)
+    target_cterm, _ = cterm_symbolic.simplify(target_cterm)
+
+    if is_bottom(init_cterm.kast, weak=True):
+        raise ValueError('Simplifying initial node led to #Bottom, are you sure your LHS is defined?')
+    if is_top(target_cterm.kast, weak=True):
+        raise ValueError('Simplifying target node led to #Bottom, are you sure your RHS is defined?')
+
+    proof.kcfg.let_node(proof.init, cterm=init_cterm)
+    proof.kcfg.let_node(proof.target, cterm=target_cterm)
 
 
 @contextmanager
