a0ea33c75724563ab7dc3143558c8f915798722c
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-06-11 05:03:55 +0000

    kevm-pyk/test_prove: remove mention of OPCODES specs

--- kevm-pyk/src/tests/integration/test_prove.py
+++ kevm-pyk/src/tests/integration/test_prove.py
@@ -54,7 +54,6 @@ def spec_files(dir_name: str, glob: str) -> tuple[Path, ...]:
 
 BENCHMARK_TESTS: Final = spec_files('benchmarks', '*-spec.k')
 FUNCTIONAL_TESTS: Final = spec_files('functional', '*-spec.k')
-OPCODES_TESTS: Final = spec_files('opcodes', '*-spec.md')
 ERC20_TESTS: Final = spec_files('erc20', '*/*-spec.k')
 EXAMPLES_TESTS: Final = spec_files('examples', '*-spec.k') + spec_files('examples', '*-spec.md')
 MCD_TESTS: Final = spec_files('mcd', '*-spec.k')
@@ -64,7 +63,6 @@ ALL_TESTS: Final = sum(
     [
         BENCHMARK_TESTS,
         FUNCTIONAL_TESTS,
-        OPCODES_TESTS,
         ERC20_TESTS,
         EXAMPLES_TESTS,
         MCD_TESTS,
