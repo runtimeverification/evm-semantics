113cf38448f6d80a98bd238c083c511f771f7d78
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-06-19 21:36:34 +0300

    test_conformance.py: parse each test in the gst files

--- kevm-pyk/src/tests/integration/test_conformance.py
+++ kevm-pyk/src/tests/integration/test_conformance.py
@@ -32,13 +32,12 @@ TEST_DIR: Final = REPO_ROOT / 'tests/ethereum-tests'
 GOLDEN: Final = (REPO_ROOT / 'tests/templates/output-success-llvm.json').read_text().rstrip()
 
 
-def _test(gst_file: Path, schedule: str, mode: str, chainid: int, usegas: bool) -> None:
+def _test(gst_file: Path, test_name: str, schedule: str, mode: str, chainid: int, usegas: bool) -> None:
     with gst_file.open() as f:
         gst_data = json.load(f)
 
-    for test_name, test in gst_data.items():
         _LOGGER.info(f'Running test: {gst_file} - {test_name}')
-        res = interpret({test_name: test}, schedule, mode, chainid, usegas, check=False)
+        res = interpret({test_name: gst_data.get(test_name)}, schedule, mode, chainid, usegas, check=False)
         _assert_exit_code_zero(res)
 
 
@@ -57,10 +56,17 @@ def _assert_exit_code_zero(pattern: Pattern) -> None:
     assert pretty == GOLDEN
 
 
-def _skipped_tests() -> set[Path]:
-    def read_test_list(path: Path) -> list[Path]:
-        return [REPO_ROOT / test_path for test_path in path.read_text().splitlines()]
+def read_test_list(path: Path) -> list[tuple[Path, str]]:
+    return [(REPO_ROOT / line.split(':')[0], line.split(':')[1]) for line in path.read_text().splitlines()]
 
+
+def read_gst_file(gst_file: Path) -> list[str]:
+    with gst_file.open() as f:
+        gst_data = json.load(f)
+    return gst_data.keys()
+
+
+def _skipped_tests() -> set[tuple[Path, str]]:
     slow_tests = read_test_list(REPO_ROOT / 'tests/slow.llvm')
     failing_tests = read_test_list(REPO_ROOT / 'tests/failing.llvm')
     return set(slow_tests + failing_tests)
@@ -69,54 +75,56 @@ def _skipped_tests() -> set[Path]:
 SKIPPED_TESTS: Final = _skipped_tests()
 
 VM_TEST_DIR: Final = TEST_DIR / 'BlockchainTests/GeneralStateTests/VMTests'
-ALL_VM_TESTS: Final = tuple(VM_TEST_DIR.glob('*/*.json'))
-VM_TESTS: Final = tuple(test_file for test_file in ALL_VM_TESTS if test_file not in SKIPPED_TESTS)
-REST_VM_TESTS: Final = tuple(test_file for test_file in ALL_VM_TESTS if test_file in SKIPPED_TESTS)
+ALL_VM_TESTS: Final = tuple((file, test_id) for file in VM_TEST_DIR.glob('*/*.json') for test_id in read_gst_file(file))
+VM_TESTS: Final = tuple(test_tuple for test_tuple in ALL_VM_TESTS if test_tuple not in SKIPPED_TESTS)
+REST_VM_TESTS: Final = tuple(test_tuple for test_tuple in ALL_VM_TESTS if test_tuple in SKIPPED_TESTS)
 
 
 @pytest.mark.parametrize(
-    'test_file',
+    'test_file, test_id',
     VM_TESTS,
-    ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in VM_TESTS],
+    ids=[f'{test_file.relative_to(VM_TEST_DIR)}: {test_id}' for test_file, test_id in VM_TESTS],
 )
-def test_vm(test_file: Path) -> None:
-    _test(test_file, 'DEFAULT', 'VMTESTS', 1, True)
+def test_vm(test_file: Path, test_id: str) -> None:
+    _test(test_file, test_id, 'DEFAULT', 'VMTESTS', 1, True)
 
 
 @pytest.mark.skip(reason='failing / slow VM tests')
 @pytest.mark.parametrize(
-    'test_file',
+    'test_file, test_id',
     REST_VM_TESTS,
-    ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in REST_VM_TESTS],
+    ids=[f'{test_file.relative_to(VM_TEST_DIR)}: {test_id}' for test_file, test_id in REST_VM_TESTS],
 )
-def test_rest_vm(test_file: Path) -> None:
-    _test(test_file, 'DEFAULT', 'VMTESTS', 1, True)
+def test_rest_vm(test_file: Path, test_id: str) -> None:
+    _test(test_file, test_id, 'DEFAULT', 'VMTESTS', 1, True)
 
 
 BCHAIN_TEST_DIR: Final = TEST_DIR / 'BlockchainTests/GeneralStateTests'
-ALL_BCHAIN_TESTS: Final = tuple(BCHAIN_TEST_DIR.glob('**/*.json'))
+ALL_BCHAIN_TESTS: Final = tuple(
+    (file, test_id) for file in BCHAIN_TEST_DIR.glob('**/*.json') for test_id in read_gst_file(file)
+)
 BCHAIN_TESTS: Final = tuple(
-    test_file for test_file in ALL_BCHAIN_TESTS if test_file not in (list(SKIPPED_TESTS) + list(ALL_VM_TESTS))
+    test_tuple for test_tuple in ALL_BCHAIN_TESTS if test_tuple not in (list(SKIPPED_TESTS) + list(ALL_VM_TESTS))
 )
 REST_BCHAIN_TESTS: Final = tuple(
-    test_file for test_file in ALL_BCHAIN_TESTS if test_file in SKIPPED_TESTS and test_file not in ALL_VM_TESTS
+    test_tuple for test_tuple in ALL_BCHAIN_TESTS if test_tuple in SKIPPED_TESTS and test_tuple not in ALL_VM_TESTS
 )
 
 
 @pytest.mark.parametrize(
-    'test_file',
+    'test_file, test_id',
     BCHAIN_TESTS,
-    ids=[str(test_file.relative_to(TEST_DIR)) for test_file in BCHAIN_TESTS],
+    ids=[f'{test_file.relative_to(TEST_DIR)}: {test_id}' for test_file, test_id in BCHAIN_TESTS],
 )
-def test_bchain(test_file: Path) -> None:
-    _test(test_file, 'SHANGHAI', 'NORMAL', 1, True)
+def test_bchain(test_file: Path, test_id: str) -> None:
+    _test(test_file, test_id, 'SHANGHAI', 'NORMAL', 1, True)
 
 
 @pytest.mark.skip(reason='failing / slow blockchain tests')
 @pytest.mark.parametrize(
-    'test_file',
+    'test_file, test_id',
     REST_BCHAIN_TESTS,
-    ids=[str(test_file.relative_to(TEST_DIR)) for test_file in REST_BCHAIN_TESTS],
+    ids=[f'{test_file.relative_to(TEST_DIR)}: {test_id}' for test_file, test_id in REST_BCHAIN_TESTS],
 )
-def test_rest_bchain(test_file: Path) -> None:
-    _test(test_file, 'SHANGHAI', 'NORMAL', 1, True)
+def test_rest_bchain(test_file: Path, test_id: str) -> None:
+    _test(test_file, test_id, 'SHANGHAI', 'NORMAL', 1, True)
