08a9ca93863676b9e66dd92b5b079efc70753d6f
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-06-27 11:13:58 +0300

    tweaks to the conformance harness

--- kevm-pyk/src/tests/integration/test_conformance.py
+++ kevm-pyk/src/tests/integration/test_conformance.py
@@ -34,12 +34,16 @@ GOLDEN: Final = (REPO_ROOT / 'tests/templates/output-success-llvm.json').read_te
 
 
 def _test(gst_file: Path, schedule: str, mode: str, chainid: int, usegas: bool) -> None:
+    skipped_gst_tests = SKIPPED_TESTS.get(gst_file, [])
+    if '*' in skipped_gst_tests:
+        pytest.skip()
+
     with gst_file.open() as f:
         gst_data = json.load(f)
 
     for test_name, test in gst_data.items():
         _LOGGER.info(f'Running test: {gst_file} - {test_name}')
-        if test_name in SKIPPED_TESTS.get(gst_file, []):
+        if test_name in skipped_gst_tests:
             continue
         res = interpret({test_name: test}, schedule, mode, chainid, usegas, check=False)
         _assert_exit_code_zero(res)
@@ -80,14 +84,13 @@ SKIPPED_TESTS: Final = _skipped_tests()
 
 VM_TEST_DIR: Final = TEST_DIR / 'BlockchainTests/GeneralStateTests/VMTests'
 ALL_VM_TESTS: Final = tuple(VM_TEST_DIR.glob('*/*.json'))
-VM_TESTS: Final = tuple(test_file for test_file in ALL_VM_TESTS if test_file not in SKIPPED_TESTS)
 REST_VM_TESTS: Final = tuple(test_file for test_file in ALL_VM_TESTS if test_file in SKIPPED_TESTS)
 
 
 @pytest.mark.parametrize(
     'test_file',
-    VM_TESTS,
-    ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in VM_TESTS],
+    ALL_VM_TESTS,
+    ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in ALL_VM_TESTS],
 )
 def test_vm(test_file: Path) -> None:
     _test(test_file, 'DEFAULT', 'VMTESTS', 1, True)
@@ -105,9 +108,7 @@ def test_rest_vm(test_file: Path) -> None:
 
 BCHAIN_TEST_DIR: Final = TEST_DIR / 'BlockchainTests/GeneralStateTests'
 ALL_BCHAIN_TESTS: Final = tuple(BCHAIN_TEST_DIR.glob('**/*.json'))
-BCHAIN_TESTS: Final = tuple(
-    test_file for test_file in ALL_BCHAIN_TESTS if test_file not in (list(SKIPPED_TESTS) + list(ALL_VM_TESTS))
-)
+BCHAIN_TESTS: Final = tuple(test_file for test_file in ALL_BCHAIN_TESTS if test_file not in list(ALL_VM_TESTS))
 REST_BCHAIN_TESTS: Final = tuple(
     test_file for test_file in ALL_BCHAIN_TESTS if test_file in SKIPPED_TESTS and test_file not in ALL_VM_TESTS
 )
