39d4afe06017ae548cc19f514a083db0f46cf02b
Andrei <16517508+anvacaru@users.noreply.github.com>
2024-07-26 04:29:29 +0300

    tests with cid 0

--- kevm-pyk/src/tests/integration/test_conformance.py
+++ kevm-pyk/src/tests/integration/test_conformance.py
@@ -31,6 +31,11 @@ sys.setrecursionlimit(10**8)
 
 TEST_DIR: Final = REPO_ROOT / 'tests/ethereum-tests'
 GOLDEN: Final = (REPO_ROOT / 'tests/templates/output-success-llvm.json').read_text().rstrip()
+TEST_FILES_WITH_CID_0: Final = (TEST_DIR / 'bchain.0.chainId').read_text().splitlines()
+
+
+def _get_chain_id(test_file: Path) -> int:
+    return 0 if test_file in TEST_FILES_WITH_CID_0 else 1
 
 
 def _test(gst_file: Path, schedule: str, mode: str, chainid: int, usegas: bool) -> None:
@@ -88,22 +93,22 @@ SKIPPED_VM_TESTS: Final = tuple(test_file for test_file in VM_TESTS if test_file
 
 
 @pytest.mark.parametrize(
-    'test_file',
-    VM_TESTS,
+    'test_file, chain_id',
+    [(test_file, _get_chain_id(test_file)) for test_file in VM_TESTS],
     ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in VM_TESTS],
 )
-def test_vm(test_file: Path) -> None:
-    _test(test_file, 'DEFAULT', 'VMTESTS', 1, True)
+def test_vm(test_file: Path, chain_id: int) -> None:
+    _test(test_file, 'DEFAULT', 'VMTESTS', chain_id, True)
 
 
 @pytest.mark.skip(reason='failing / slow VM tests')
 @pytest.mark.parametrize(
-    'test_file',
-    SKIPPED_VM_TESTS,
+    'test_file, chain_id',
+    [(test_file, _get_chain_id(test_file)) for test_file in SKIPPED_VM_TESTS],
     ids=[str(test_file.relative_to(VM_TEST_DIR)) for test_file in SKIPPED_VM_TESTS],
 )
-def test_rest_vm(test_file: Path) -> None:
-    _test(test_file, 'DEFAULT', 'VMTESTS', 1, True)
+def test_rest_vm(test_file: Path, chain_id: int) -> None:
+    _test(test_file, 'DEFAULT', 'VMTESTS', chain_id, True)
 
 
 ALL_TEST_DIR: Final = TEST_DIR / 'BlockchainTests/GeneralStateTests'
@@ -113,19 +118,19 @@ SKIPPED_BCHAIN_TESTS: Final = tuple(test_file for test_file in BCHAIN_TESTS if t
 
 
 @pytest.mark.parametrize(
-    'test_file',
-    BCHAIN_TESTS,
+    'test_file, chain_id',
+    [(test_file, _get_chain_id(test_file)) for test_file in BCHAIN_TESTS],
     ids=[str(test_file.relative_to(ALL_TEST_DIR)) for test_file in BCHAIN_TESTS],
 )
-def test_bchain(test_file: Path) -> None:
-    _test(test_file, 'CANCUN', 'NORMAL', 1, True)
+def test_bchain(test_file: Path, chain_id: int) -> None:
+    _test(test_file, 'CANCUN', 'NORMAL', chain_id, True)
 
 
 @pytest.mark.skip(reason='failing / slow blockchain tests')
 @pytest.mark.parametrize(
-    'test_file',
-    SKIPPED_BCHAIN_TESTS,
+    'test_file, chain_id',
+    [(test_file, _get_chain_id(test_file)) for test_file in SKIPPED_BCHAIN_TESTS],
     ids=[str(test_file.relative_to(ALL_TEST_DIR)) for test_file in SKIPPED_BCHAIN_TESTS],
 )
-def test_rest_bchain(test_file: Path) -> None:
-    _test(test_file, 'CANCUN', 'NORMAL', 1, True)
+def test_rest_bchain(test_file: Path, chain_id: int) -> None:
+    _test(test_file, 'CANCUN', 'NORMAL', chain_id, True)
