fcf947e1d530d608d87bfdc8c86144e6fd2c3466
rv-jenkins <admin@runtimeverification.com>
2024-08-28 01:41:14 -0600

    Update dependency: kevm-pyk/src/kevm_pyk/kproj/plugin (#2598)
    * kevm-pyk/src/kevm_pyk/kproj/plugin: Set Version 69fdf830e3a98a8aa1118f84bd83723f1149468e
    
    * kevm-pyk/: sync poetry files pyk version 7.1.121
    
    * deps/blockchain-k-plugin_release: sync release file version 69fdf830e3a98a8aa1118f84bd83723f1149468e
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     poetry2nix.follows = "pyk/poetry2nix";
     blockchain-k-plugin = {
       url =
-        "github:runtimeverification/blockchain-k-plugin/153c4a03f2493117467440386e549d8c748db6bf";
+        "github:runtimeverification/blockchain-k-plugin/69fdf830e3a98a8aa1118f84bd83723f1149468e";
       inputs.flake-utils.follows = "k-framework/flake-utils";
       inputs.nixpkgs.follows = "k-framework/nixpkgs";
     };
