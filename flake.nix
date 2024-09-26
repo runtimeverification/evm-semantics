1bd8e09dec9bc07aee77a99ca2cee5457df5b3d2
rv-jenkins <admin@runtimeverification.com>
2024-09-26 06:29:31 -0600

    Update dependency: deps/k_release (#2631)
    * deps/k_release: Set Version 7.1.151
    
    * kevm-pyk/: sync poetry files pyk version 7.1.151
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for the KEVM Semantics";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.1.150";
+    k-framework.url = "github:runtimeverification/k/v7.1.151";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
