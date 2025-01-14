b79c1a3d572619e4dfb730504658a009cfcffb2c
Stevengre <zhaojianhong96@gmail.com>
2025-01-14 03:16:50 +0000

    summarized `MUL`

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -32,7 +32,7 @@ from pyk.proof.show import APRProofShow
 from pyk.proof.tui import APRProofViewer
 from pyk.utils import FrozenDict, hash_str, single
 
-from kevm_pyk.summarizer import analyze_proof, batch_summarize, summarize
+from kevm_pyk.summarizer import analyze_proof, batch_summarize, stack_needed, summarize
 
 from . import VERSION, config
 from .cli import _create_argument_parser, generate_options, get_argument_type_setter, get_option_string_destination
@@ -638,9 +638,8 @@ def exec_kast(options: KastOptions) -> None:
 
 def exec_summarize(options: ProveOptions) -> None:
     # batch_summarize()
-    summarize('ADD')
+    summarize('MUL')
     # analyze_proof('STOP', 11)
-
 
 # Helpers
 
