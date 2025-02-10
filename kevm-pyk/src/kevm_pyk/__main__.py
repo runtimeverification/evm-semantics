173b85c2000fb9512d3d839c884b17f29338dd28
Stevengre <zhaojianhong96@gmail.com>
2025-02-10 05:58:41 +0000

    simplify the implementation & fix the summarization for BALANCE

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -32,7 +32,7 @@ from pyk.proof.show import APRProofShow
 from pyk.proof.tui import APRProofViewer
 from pyk.utils import FrozenDict, hash_str, single
 
-from kevm_pyk.summarizer import batch_summarize
+from kevm_pyk.summarizer import analyze_proof, summarize
 
 from . import VERSION, config
 from .cli import _create_argument_parser, generate_options, get_argument_type_setter, get_option_string_destination
@@ -637,7 +637,9 @@ def exec_kast(options: KastOptions) -> None:
 
 
 def exec_summarize(options: ProveOptions) -> None:
-    batch_summarize()
+    # analyze_proof('BALANCE_1', 3)
+    summarize('BALANCE')
+    # batch_summarize()
 
 
 # Helpers
