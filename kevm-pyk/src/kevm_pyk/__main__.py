ee6ffac4a7ecb45924262106c731ebd0f9ae0573
Stevengre <zhaojianhong96@gmail.com>
2025-02-11 02:58:50 +0000

    make format & make check

--- kevm-pyk/src/kevm_pyk/__main__.py
+++ kevm-pyk/src/kevm_pyk/__main__.py
@@ -32,7 +32,7 @@ from pyk.proof.show import APRProofShow
 from pyk.proof.tui import APRProofViewer
 from pyk.utils import FrozenDict, hash_str, single
 
-from kevm_pyk.summarizer import analyze_proof, summarize
+from kevm_pyk.summarizer import batch_summarize
 
 from . import VERSION, config
 from .cli import _create_argument_parser, generate_options, get_argument_type_setter, get_option_string_destination
@@ -638,11 +638,8 @@ def exec_kast(options: KastOptions) -> None:
 
 def exec_summarize(options: ProveOptions) -> None:
     # analyze_proof('EXTCODESIZE_OWISE_1', 3)
-    # summarize('EXTCODESIZE')
-    # summarize('EXTCODECOPY')
-    # summarize('EXTCODEHASH')
-    summarize('SELFBALANCE')
-    # batch_summarize()
+    # summarize('JUMP')
+    batch_summarize()
 
 
 # Helpers
