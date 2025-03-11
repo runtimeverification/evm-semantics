4103f0541c8810e30e101cc2840f9b9a717ecc61
Stevengre <zhaojianhong96@gmail.com>
2025-03-11 15:43:25 +0000

    Skip time-consuming opcode summarization tests in test_summarize.py

--- kevm-pyk/src/tests/integration/test_summarize.py
+++ kevm-pyk/src/tests/integration/test_summarize.py
@@ -9,7 +9,7 @@ from kevm_pyk.summarizer import OPCODES, OPCODES_SUMMARY_STATUS, get_summary_sta
 def test_summarize(opcode: str) -> None:
     if get_summary_status(opcode) != 'PASSED':
         pytest.skip(f'{opcode} status: {OPCODES_SUMMARY_STATUS[opcode]}')
-    
+
     if opcode in ['DUP', 'SWAP', 'LOG']:
         pytest.skip(f'{opcode} summarization is time-consuming')
 
