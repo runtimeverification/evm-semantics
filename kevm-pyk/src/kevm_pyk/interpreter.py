a2bbdf0a717c7f630cef6d9f8f9865b2a1fe5cbf
Stevengre <zhaojianhong96@gmail.com>
2025-03-17 08:53:19 +0000

    Refactor interpreter error handling and update _interpret function
    - Introduced error handling for the interpreter to raise a RuntimeError with detailed information if the subprocess fails or returns empty stdout.
    - Updated the _interpret function to accept a 'check' parameter and utilize llvm_interpret_raw for improved process execution.

--- kevm-pyk/src/kevm_pyk/interpreter.py
+++ kevm-pyk/src/kevm_pyk/interpreter.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
 
 from pyk.kdist import kdist
 from pyk.kore.parser import KoreParser
-from pyk.utils import run_process_2
+from pyk.ktool.krun import llvm_interpret_raw
 
 from .gst_to_kore import filter_gst_keys, gst_to_kore
 
@@ -16,21 +17,18 @@ if TYPE_CHECKING:
 
 
 def interpret(gst_data: Any, schedule: str, mode: str, chainid: int, usegas: bool, *, check: bool = True) -> Pattern:
-    proc_res = _interpret(gst_data, schedule, mode, chainid, usegas)
+    try:
+        res = _interpret(gst_data, schedule, mode, chainid, usegas, check)
+    except CalledProcessError as err:
+        raise RuntimeError(f'Interpreter failed with status {err.returncode}: {err.stderr}') from err
 
-    if check:
-        proc_res.check_returncode()
+    if res.stdout == '':
+        raise RuntimeError(f'Interpreter returned empty stdout: {res.stderr}')
 
-    if len(proc_res.stderr) > 0:
-        raise Exception(proc_res.stderr)
-
-    kore = KoreParser(proc_res.stdout).pattern()
-    return kore
+    return KoreParser(res.stdout).pattern()
 
 
-def _interpret(gst_data: Any, schedule: str, mode: str, chainid: int, usegas: bool) -> CompletedProcess:
+def _interpret(gst_data: Any, schedule: str, mode: str, chainid: int, usegas: bool, check: bool) -> CompletedProcess:
     gst_data_filtered = filter_gst_keys(gst_data)
-    interpreter = kdist.get('evm-semantics.llvm') / 'interpreter'
     init_kore = gst_to_kore(gst_data_filtered, schedule, mode, chainid, usegas)
-    proc_res = run_process_2([str(interpreter), '/dev/stdin', '-1', '/dev/stdout'], input=init_kore.text, check=False)
-    return proc_res
+    return llvm_interpret_raw(kdist.get('evm-semantics.llvm-summary'), init_kore.text, check=check)
