df1138dd555d6f8ca7c88c4040d8f391e400b300
Stevengre <zhaojianhong96@gmail.com>
2025-03-17 09:27:25 +0000

    Refactor interpreter function to streamline GST data interpretation
    - Simplified the `interpret` function by removing error handling for subprocess calls and directly utilizing `llvm_interpret`.
    - Updated the `interpret` function to improve clarity and efficiency by integrating the filtering and conversion of GST data into a single line.
    - Enhanced documentation to clarify the purpose of the `interpret` function.

--- kevm-pyk/src/kevm_pyk/interpreter.py
+++ kevm-pyk/src/kevm_pyk/interpreter.py
@@ -1,34 +1,19 @@
 from __future__ import annotations
 
-from subprocess import CalledProcessError
 from typing import TYPE_CHECKING
 
 from pyk.kdist import kdist
-from pyk.kore.parser import KoreParser
-from pyk.ktool.krun import llvm_interpret_raw
+from pyk.ktool.krun import llvm_interpret
 
 from .gst_to_kore import filter_gst_keys, gst_to_kore
 
 if TYPE_CHECKING:
-    from subprocess import CompletedProcess
     from typing import Any
 
     from pyk.kore.syntax import Pattern
 
 
 def interpret(gst_data: Any, schedule: str, mode: str, chainid: int, usegas: bool, *, check: bool = True) -> Pattern:
-    try:
-        res = _interpret(gst_data, schedule, mode, chainid, usegas, check)
-    except CalledProcessError as err:
-        raise RuntimeError(f'Interpreter failed with status {err.returncode}: {err.stderr}') from err
-
-    if res.stdout == '':
-        raise RuntimeError(f'Interpreter returned empty stdout: {res.stderr}')
-
-    return KoreParser(res.stdout).pattern()
-
-
-def _interpret(gst_data: Any, schedule: str, mode: str, chainid: int, usegas: bool, check: bool) -> CompletedProcess:
-    gst_data_filtered = filter_gst_keys(gst_data)
-    init_kore = gst_to_kore(gst_data_filtered, schedule, mode, chainid, usegas)
-    return llvm_interpret_raw(kdist.get('evm-semantics.llvm-summary'), init_kore.text, check=check)
+    """Interpret the given GST data using the LLVM backend."""
+    init_kore = gst_to_kore(filter_gst_keys(gst_data), schedule, mode, chainid, usegas)
+    return llvm_interpret(kdist.get('evm-semantics.llvm-summary'), init_kore, check=check)
