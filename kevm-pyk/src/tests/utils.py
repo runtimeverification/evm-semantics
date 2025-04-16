371a69bb3de940eed89dc3399f78ddf412ff344b
Andrei <16517508+anvacaru@users.noreply.github.com>
2025-04-16 17:31:41 +0300

    add pyupgrade suggestions

--- kevm-pyk/src/tests/utils.py
+++ kevm-pyk/src/tests/utils.py
@@ -15,7 +15,8 @@ from pyk.kore.tools import PrintOutput, kore_print
 from kevm_pyk.interpreter import interpret
 
 if TYPE_CHECKING:
-    from typing import Callable, Final
+    from typing import Final
+    from collections.abc import Callable
 
     from pyk.kore.syntax import Pattern
 
