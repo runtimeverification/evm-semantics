f70b5102aabbc2d6bbe6e0643be100867ba7ed35
devops <devops@runtimeverification.com>
2025-05-12 21:33:28 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for the KEVM Semantics";
 
   inputs = {
-    k-framework.url = "github:runtimeverification/k/v7.1.251";
+    k-framework.url = "github:runtimeverification/k/v7.1.252";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
