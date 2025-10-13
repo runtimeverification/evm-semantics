55477b52b940e57bae44f0531052eaa30a32b903
Julian Kuners <julian.kuners@gmail.com>
2025-10-13 09:30:52 +0100

    add pyproject-nix overlay (#2802)

--- nix/kevm-pyk/default.nix
+++ nix/kevm-pyk/default.nix
@@ -1,47 +1,33 @@
 {
   lib,
   callPackage,
-  stdenvNoCC,
-
-  blockchain-k-plugin-src,
 
   pyproject-nix,
   pyproject-build-systems,
-  uv2nix,
+  kevm-pyk-pyproject,
+  pyproject-overlays ? [ ],
 
   python
 }:
 let
+  inherit (kevm-pyk-pyproject) lockFileOverlay workspace;
+
   pyproject-util = callPackage pyproject-nix.build.util {};
   pyproject-packages = callPackage pyproject-nix.build.packages {
     inherit python;
   };
 
-  src = callPackage ../kevm-source { };
-
-  # load a uv workspace from a workspace root
-  workspace = uv2nix.lib.workspace.loadWorkspace {
-    workspaceRoot = "${src}/kevm-pyk";
-  };
-
-  # create overlay
-  lockFileOverlay = workspace.mkPyprojectOverlay {
-    # prefer "wheel" over "sdist" due to maintance overhead
-    # there is no bundled set of overlays for "sdist" in uv2nix, in contrast to poetry2nix
-    sourcePreference = "wheel";
-  };
-
   buildSystemsOverlay = import ./build-systems-overlay.nix;
 
   # construct package set
-  pythonSet = pyproject-packages.overrideScope (lib.composeManyExtensions [
+  pythonSet = pyproject-packages.overrideScope (lib.composeManyExtensions ([
     # make build tools available by default as these are not necessarily specified in python lock files
     pyproject-build-systems.overlays.default
     # include all packages from the python lock file
     lockFileOverlay
     # add build system overrides to certain python packages
     buildSystemsOverlay
-  ]);
+  ] ++ pyproject-overlays));
 in pyproject-util.mkApplication {
   # default dependancy group enables no optional dependencies and no dependency-groups
   venv = pythonSet.mkVirtualEnv "kevm-pyk-env" workspace.deps.default;
