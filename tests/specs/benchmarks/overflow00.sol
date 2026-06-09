87027582ada500e733932a89c858f81268b50bb7
rv-jenkins <devops@runtimeverification.com>
2026-06-09 14:27:51 +0000

    gh-pages: Updated the website

--- tests/specs/benchmarks/overflow00.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.0;
-contract overflow00 {
-    function execute(uint a0) public returns(uint) {
-        uint n = a0 + 1;
-        return n;
-    }
-}
--- tests/specs/benchmarks/requires01.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.24;
-contract requires01 {
-    function execute(uint256 a0) public returns (address) {
-        require(a0 > 0);
-        return 5;
-    }
-}
-
